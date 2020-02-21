0de28ecfd2395f20dceb73e4f4cdd26c001160b0
Yoya Mesaki <yoyamssk@gmail.com>
2020-02-22 00:33:38 +0900

    messageFormStateのexport重複削除

--- src/store/ui/messageContextMenu/index.ts
+++ src/store/ui/messageContextMenu/index.ts
@@ -4,12 +4,6 @@ import { getters } from './getters'
 import { mutations } from './mutations'
 import { actions } from './actions'
 
-export interface MessageFormState {
-  filesToUpload: File[]
-  message: string
-  selection: Selection
-}
-
 export const messageContextMenu = createModule({
   namespaced: true,
   state,
