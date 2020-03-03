49cae5036d89a7e9c2d59106256f1388c45d0edb
Yoya Mesaki <yoyamssk@gmail.com>
2020-03-03 23:41:50 +0900

    テーマをstoreに移動

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { createComponent } from '@vue/composition-api'
+import { createComponent, reactive } from '@vue/composition-api'
 import { ThemeProvider } from 'vue-styled-components'
 import HelloWorld from '@/components/HelloWorld'
+import store from './store'
 
 export default createComponent({
   name: 'App',
   setup() {
+    const state = reactive({
+      color: store.state.app.theme.accent.primary
+    })
     return () => (
       <div id="app">
-        <ThemeProvider
-          theme={{
-            accent: {
-              primary: '#005BAC',
-              notification: '#F2994A',
-              online: '#28F0E4'
-            },
-            background: {
-              primary: '#FFFFFF',
-              secondary: '#F6F7F9',
-              tertiary: '#ECEFF3'
-            },
-            ui: {
-              primary: '#525E67',
-              secondary: '#828E96',
-              tertiary: '#ECEFF3'
-            },
-            text: {
-              primary: '#333333',
-              secondary: '#79797A'
-            }
-          }}
-        >
+        <ThemeProvider theme={store.state.app.theme}>
           <HelloWorld msg="traQ" />
           <div id="nav">
             <router-link to="/">Home</router-link> |
