2818e8fda7ab6a8e91cdffb4ac0f398088632e41
sapphi-red <green@sapphi.red>
2020-03-15 15:23:24 +0900

    node-traqの移行

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,13 +1,13 @@
-import { Apis as V3Apis } from 'traq-api-v3'
+import { Apis } from '@traptitech/traq'
 
-const apis = new V3Apis({
+const apis = new Apis({
   basePath: '/api/v3'
 })
 
-export * from 'traq-api-v3'
+export * from '@traptitech/traq'
 export default apis
 
-export const WEBSOCKET_ENDPOINT = '/api/1.0/ws'
+export const WEBSOCKET_ENDPOINT = '/api/v3/ws'
 
 export enum ChannelViewerStateEnum {
   VIEWSTATE_NONE = 'none',
