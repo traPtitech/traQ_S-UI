4d1f222277259c10dff4280d695cb14fd53b555a
sapphi-red <green@sapphi.red>
2020-03-15 15:27:19 +0900

    fix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -8,9 +8,3 @@ export * from '@traptitech/traq'
 export default apis
 
 export const WEBSOCKET_ENDPOINT = '/api/v3/ws'
-
-export enum ChannelViewerStateEnum {
-  VIEWSTATE_NONE = 'none',
-  VIEWSTATE_MONITORING = 'monitoring',
-  VIEWSTATE_EDITING = 'editing'
-}
