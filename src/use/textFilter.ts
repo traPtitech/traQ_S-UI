91f68933f7d95ce941a2d37982ef477db54f8dfd
Yoya Mesaki <yoyamssk@gmail.com>
2020-03-30 17:20:21 +0900

    add 1-letter matching

--- src/use/textFilter.ts
+++ src/use/textFilter.ts
@@ -4,21 +4,31 @@ const useTextFilter = <T, K extends keyof T>(
   items: Ref<readonly T[]>,
   searchTargetKey: T[K] extends string ? K : never
 ) => {
+  // NOTE: stringでなければ呼び出しが通らないのでキャストできる
+  const oneLetterItems = computed(() =>
+    items.value.filter(
+      item => ((item[searchTargetKey] as unknown) as string).length === 1
+    )
+  )
   const state = reactive({
     query: '',
     filteredItems: computed((): readonly T[] => {
-      // TODO: 1文字完全一致のみの対応
       if (state.query.length === 0) {
         return items.value
-      } else {
-        return items.value.filter(
-          // NOTE: stringでなければ通らないのでキャストしてしまう
+      }
+      if (state.query.length === 1) {
+        return oneLetterItems.value.filter(
           item =>
-            ((item[searchTargetKey] as unknown) as string)?.indexOf(
+            ((item[searchTargetKey] as unknown) as string).indexOf(
               state.query
-            ) >= 0
+            ) === 0
         )
       }
+      return items.value.filter(
+        item =>
+          ((item[searchTargetKey] as unknown) as string).indexOf(state.query) >=
+          0
+      )
     })
   })
   const setQuery = (query: string) => {
