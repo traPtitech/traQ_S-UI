884c9c18e6e43cf0052480d5e5a1fd1cae145ebf
sapphi-red <green@sapphi.red>
2020-04-05 22:51:30 +0900

    modern mode refs #240

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,25 @@
-module.exports = {
-  presets: ['@vue/app'],
+const options = {
   plugins: [
     '@babel/plugin-proposal-optional-chaining',
     '@babel/plugin-proposal-nullish-coalescing-operator'
   ]
+}
+
+const vueCliPreset = require('@vue/cli-plugin-babel/preset')
+
+module.exports = api => {
+  api.cache.using(() => process.env.VUE_CLI_MODERN_BUILD)
+  api.cache.using(() => process.env.NODE_ENV)
+
+  const isModern = process.env.VUE_CLI_MODERN_BUILD === 'true'
+
+  if (isModern) {
+    delete process.env.VUE_CLI_MODERN_BUILD
+  }
+  const config = vueCliPreset(api)
+  if (isModern) {
+    process.env.VUE_CLI_MODERN_BUILD = 'true'
+  }
+
+  return { ...options, ...config }
 }
