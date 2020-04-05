c2398153b662ff83e856b42a3876ed96af345b62
sapphi-red <green@sapphi.red>
2020-04-05 17:36:26 +0900

    スタンプ設定

---
+++ src/components/Main/Modal/SettingModal/StampTab/Stamp.vue
@@ -0,0 +1,39 @@
+<template>
+  <div :class="$style.container">
+    <img :src="url" :class="$style.stamp" />
+    <span>:{{ stamp.name }}:</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed, PropType } from '@vue/composition-api'
+import apis, { buildFilePath, Stamp } from '@/lib/api'
+import store from '@/store'
+
+export default defineComponent({
+  name: 'Stamp',
+  props: {
+    stamp: {
+      type: Object as PropType<Stamp>,
+      required: true
+    }
+  },
+  setup(prop) {
+    const url = computed(() => buildFilePath(prop.stamp.fileId))
+
+    return { url }
+  }
+})
+</script>
+
+<style lang="scss" module>
+.container {
+  display: flex;
+  align-items: center;
+}
+
+.stamp {
+  height: 40px;
+  width: 40px;
+}
+</style>
--- src/components/Main/Modal/SettingModal/StampTab/StampTab.vue
+++ src/components/Main/Modal/SettingModal/StampTab/StampTab.vue
@@ -1,18 +1,92 @@
 <template>
   <section :class="$style.container">
-    <p>スタンプ登録</p>
-    <p>スタンプ編集</p>
-    <p>スタンプパレット</p>
+    <div>
+      <h3>スタンプ新規登録</h3>
+      <image-upload
+        @input="onNewImgSet"
+        :destroyFlag="newDestroyFlag"
+        @destroyed="onNewDestroyed"
+      />
+      <label>
+        スタンプ名
+        <div :class="$style.stampNameInput">
+          <input type="text" v-model="newStampName" />
+        </div>
+      </label>
+      <button @click="createStamp">新規登録</button>
+    </div>
+    <div>
+      <h3>スタンプ編集</h3>
+      <p>スタンプ選択できる何か</p>
+      <div v-for="stamp in myStamps" :key="stamp.id">
+        <stamp :stamp="stamp" />
+      </div>
+      <image-upload
+        @input="onNewImgSet"
+        :destroyFlag="newDestroyFlag"
+        @destroyed="onNewDestroyed"
+      />
+    </div>
+    <div>
+      <h3>スタンプパレット</h3>
+      <p title="4/1には実装されるよ">実装予定</p>
+    </div>
   </section>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent, ref, computed } from '@vue/composition-api'
+import apis from '@/lib/api'
+import ImageUpload from '../ImageUpload.vue'
+import store from '@/store'
+import Stamp from './Stamp.vue'
 
 export default defineComponent({
   name: 'StampTab',
   setup() {
-    return {}
+    const newImgData = ref<Blob>()
+    const onNewImgSet = (file: Blob) => {
+      newImgData.value = file
+    }
+
+    const newDestroyFlag = ref(false)
+    const onNewDestroyed = () => {
+      newDestroyFlag.value = false
+    }
+
+    const newStampName = ref('')
+
+    const createStamp = async () => {
+      try {
+        // TODO: loading
+        await apis.createStamp(newStampName.value, newImgData.value)
+        newImgData.value = undefined
+        newStampName.value = ''
+        newDestroyFlag.value = true
+      } catch (e) {
+        // TODO: error
+      }
+    }
+
+    const myUserId = computed(() => store.state.domain.me.detail!.id)
+    const myStamps = computed(() =>
+      Object.values(store.state.entities.stamps).filter(
+        stamp => stamp.creatorId === myUserId.value
+      )
+    )
+
+    return {
+      onNewImgSet,
+      newDestroyFlag,
+      onNewDestroyed,
+      newStampName,
+      createStamp,
+      myStamps
+    }
+  },
+  components: {
+    ImageUpload,
+    Stamp
   }
 })
 </script>
@@ -21,5 +95,14 @@ export default defineComponent({
 .container {
   padding: 8px 16px;
   overflow: hidden;
+}
+
+.stampNameInput {
+  &::before {
+    content: ':';
+  }
+  &::after {
+    content: ':';
+  }
 }
 </style>
