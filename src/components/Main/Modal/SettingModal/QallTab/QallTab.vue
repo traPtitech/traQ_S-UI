ebb427a4a6accff7c29e084669b4d041d6d9128a
sapphi-red <green@sapphi.red>
2020-04-05 17:15:31 +0900

    Qall設定

--- src/components/Main/Modal/SettingModal/QallTab/QallTab.vue
+++ src/components/Main/Modal/SettingModal/QallTab/QallTab.vue
@@ -1,17 +1,107 @@
 <template>
   <section :class="$style.container">
-    <p>RTC機能の有効化</p>
-    <p>入力デバイス</p>
+    <div>
+      <h3>RTC機能を有効にする (β)</h3>
+      <p>
+        通話などのリアルタイムコミュニケーション機能を有効化します<br />
+        マイクなどへのアクセス許可が必要です
+      </p>
+      <input type="checkbox" v-model="state.isEnabled" />
+    </div>
+    <div v-if="rtcSettings.isEnabled">
+      <h3>入力デバイス</h3>
+      <select
+        v-if="!fetchFailed && audioInputDevices.length > 0"
+        v-model="state.audioInputDeviceId"
+      >
+        <option
+          v-for="device in audioInputDevices"
+          :key="device.deviceId"
+          :value="device.deviceId"
+        >
+          {{ device.label }}
+        </option>
+      </select>
+      <p v-else>
+        デバイスが取得できませんでした
+      </p>
+    </div>
+    <button v-if="isStateChanged" @click="setState">更新</button>
   </section>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
+import {
+  defineComponent,
+  computed,
+  reactive,
+  ref,
+  watchEffect
+} from '@vue/composition-api'
+import store from '@/store'
+import useStateDiff from '../use/stateDiff'
+
+const useDevicesInfo = (state: { isEnabled: boolean }) => {
+  const devices = ref<MediaDeviceInfo[]>([])
+  const fetchFailed = ref(false)
+
+  const fetchDeviceList = async () => {
+    try {
+      devices.value = await navigator.mediaDevices.enumerateDevices()
+    } catch (e) {
+      fetchFailed.value = true
+    }
+    if (devices.value.length === 0 || devices.value[0].label === '') {
+      fetchFailed.value = true
+    }
+  }
+
+  if (state.isEnabled) {
+    fetchDeviceList()
+  }
+
+  watchEffect(async () => {
+    if (state.isEnabled) {
+      // offからonになったときは許可を求める
+      await navigator.mediaDevices.getUserMedia({ audio: true })
+      fetchDeviceList()
+    }
+  })
+
+  const audioInputDevices = computed(() =>
+    devices.value.filter(d => d.kind === 'audioinput')
+  )
+  const audioOutputDevices = computed(() =>
+    devices.value.filter(d => d.kind === 'audiooutput')
+  )
+  const videoInputDevices = computed(() =>
+    devices.value.filter(d => d.kind === 'videoinput')
+  )
+
+  return {
+    devices,
+    fetchFailed,
+    audioInputDevices,
+    audioOutputDevices,
+    videoInputDevices
+  }
+}
 
 export default defineComponent({
   name: 'QallTab',
   setup() {
-    return {}
+    const rtcSettings = computed(() => store.state.app.rtcSettings)
+    const state = reactive({ ...rtcSettings.value })
+    const { hasDiff } = useStateDiff<typeof store.state.app.rtcSettings>()
+    const isStateChanged = computed(() => hasDiff(state, rtcSettings))
+
+    const setState = () => {
+      store.commit.app.rtcSettings.setAll(state)
+    }
+
+    const devicesInfo = useDevicesInfo(state)
+
+    return { rtcSettings, state, isStateChanged, setState, ...devicesInfo }
   }
 })
 </script>
--- src/store/app/rtcSettings/mutations.ts
+++ src/store/app/rtcSettings/mutations.ts
@@ -1,4 +1,10 @@
 import { defineMutations } from 'direct-vuex'
 import { S } from './state'
 
-export const mutations = defineMutations<S>()({})
+export const mutations = defineMutations<S>()({
+  setAll<K extends keyof S>(state: S, newState: Partial<S>) {
+    ;(Object.keys(newState) as K[]).forEach(key => {
+      state[key] = newState[key] as S[K]
+    })
+  }
+})
