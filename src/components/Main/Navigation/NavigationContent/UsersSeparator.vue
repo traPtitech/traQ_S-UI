391f51f57de652a03f5f2a0cf0a95e0ac2eb7638
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2020-04-06 19:57:50 +0900

    UsersSeparatorにrounded-triangleを追加

--- src/components/Main/Navigation/NavigationContent/UsersSeparator.vue
+++ src/components/Main/Navigation/NavigationContent/UsersSeparator.vue
@@ -1,17 +1,24 @@
 <template>
   <div :class="$style.container" :style="containerStyle">
     {{ props.name }}
+    <div :class="$style.line"></div>
+    <icon name="rounded-triangle" :color="iconColor" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent, computed } from '@vue/composition-api'
+import store from '@/store'
 import { makeStyles } from '@/lib/styles'
+import Icon from '@/components/UI/Icon.vue'
 
 type Props = { name: string }
 
 export default defineComponent({
   name: 'UsersSeparator',
+  components: {
+    Icon
+  },
   props: { name: { type: String, default: '' } },
   setup(props: Props) {
     return {
@@ -19,7 +26,8 @@ export default defineComponent({
       containerStyle: makeStyles(theme => ({
         borderColor: theme.ui.tertiary,
         color: theme.ui.secondary
-      }))
+      })),
+      iconColor: computed(() => store.state.app.theme.ui.tertiary)
     }
   }
 })
@@ -30,16 +38,18 @@ export default defineComponent({
   display: flex;
   align-items: center;
   font-size: 0.875rem;
-  &::after {
-    content: '';
-    display: block;
-    margin-left: 8px;
-    width: 100%;
-    border-bottom: {
-      style: solid;
-      width: 2px;
-      color: inherit;
-    }
+  justify-content: space-between;
+}
+.line {
+  content: '';
+  display: block;
+  margin-left: 8px;
+  margin-right: 8px;
+  width: 100%;
+  border-bottom: {
+    style: solid;
+    width: 2px;
+    color: inherit;
   }
 }
 </style>
