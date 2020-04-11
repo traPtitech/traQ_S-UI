549cb43a6ca2b527e2e50292565084a544542575
Yoya Mesaki <yoyamssk@gmail.com>
2020-04-11 13:40:32 +0900

    Impl commom modal frame

---
+++ src/components/Main/Modal/Common/ModalFrame.vue
@@ -0,0 +1,69 @@
+<template>
+  <div :class="$style.container" :style="styles.container">
+    <common-modal-header
+      :class="$style.header"
+      :icon-name="iconName"
+      :icon-mdi="iconMdi"
+      :title="title"
+      :subtitle="subtitle"
+    />
+    <div :class="$style.body">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed, reactive } from '@vue/composition-api'
+import store from '@/store'
+import { makeStyles } from '@/lib/styles'
+import CommonModalHeader from './ModalHeader.vue'
+
+const useStyles = () =>
+  reactive({
+    container: makeStyles(theme => ({
+      background: theme.background.primary,
+      borderColor: theme.background.secondary
+    }))
+  })
+
+export default defineComponent({
+  name: 'ModalFrame',
+  components: {
+    CommonModalHeader
+  },
+  props: {
+    iconMdi: { type: Boolean, default: false },
+    iconName: { type: String, required: true },
+    title: { type: String, required: true },
+    subtitle: { type: String, default: '' }
+  },
+  setup() {
+    const styles = useStyles()
+    return { styles }
+  }
+})
+</script>
+
+<style lang="scss" module>
+.container {
+  display: flex;
+  flex-flow: column nowrap;
+  max-height: 368px;
+  max-width: 360px;
+  width: 100%;
+  height: 100%;
+  border-radius: 4px;
+  overflow: hidden;
+  border: {
+    style: solid;
+    width: 2px;
+  }
+}
+.header {
+  flex-shrink: 0;
+}
+.body {
+  padding: 16px 24px;
+}
+</style>
---
+++ src/components/Main/Modal/NotificationModal/NotificationModal.vue
@@ -0,0 +1,39 @@
+<template>
+  <modal-frame
+    title="通知設定"
+    :subtitle="`#${currentChannelPathString}`"
+    icon-mdi
+    icon-name="bell"
+    >通知</modal-frame
+  >
+</template>
+
+<script lang="ts">
+import { defineComponent, computed, reactive } from '@vue/composition-api'
+import store from '@/store'
+import { makeStyles } from '@/lib/styles'
+import useCurrentChannelPath from '@/use/currentChannelPath'
+import ModalFrame from '../Common/ModalFrame.vue'
+
+const useStyles = () =>
+  reactive({
+    container: makeStyles(theme => ({}))
+  })
+
+export default defineComponent({
+  name: 'NotificationModal',
+  components: {
+    ModalFrame
+  },
+  setup() {
+    const styles = useStyles()
+    const { currentChannelPathString } = useCurrentChannelPath()
+    return { styles, currentChannelPathString }
+  }
+})
+</script>
+
+<style lang="scss" module>
+.container {
+}
+</style>
