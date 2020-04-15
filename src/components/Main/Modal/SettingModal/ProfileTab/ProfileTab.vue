c460f3bf5b780b9b25ec530ce9756086f877f4cb
sapphi-red <green@sapphi.red>
2020-04-15 16:18:52 +0900

    ProfileTabをuseImageUploadで書き換え

--- src/components/Main/Modal/SettingModal/ProfileTab/ProfileTab.vue
+++ src/components/Main/Modal/SettingModal/ProfileTab/ProfileTab.vue
@@ -4,9 +4,9 @@
       <h3>アイコン</h3>
       <user-icon :user-id="detail.id" :size="100" :prevent-modal="true" />
       <image-upload
-        @input="onImgSet"
-        :destroy-flag="destroyFlag"
-        @destroyed="onDestroyed"
+        @input="onNewImgSet"
+        :destroy-flag="imageUploadState.destroyFlag"
+        @destroyed="onNewDestroyed"
         rounded
       />
     </div>
@@ -51,6 +51,7 @@ import apis from '@/lib/api'
 import useStateDiff from '../use/stateDiff'
 import UserIcon from '@/components/UI/UserIcon.vue'
 import ImageUpload from '../ImageUpload.vue'
+import useImageUpload from '../use/imageUpload'
 import useChannelPath from '@/use/channelPath'
 import FormInput from '@/components/UI/FormInput.vue'
 import FormSelector from '@/components/UI/FormSelector.vue'
@@ -100,23 +101,22 @@ export default defineComponent({
       () => hasDiff(state, detail) || isHomeChannelChanged.value
     )
 
-    const imgData = ref<Blob>()
-    const onImgSet = (file: Blob) => {
-      imgData.value = file
-    }
-
-    const destroyFlag = ref(false)
-    const onDestroyed = () => {
-      destroyFlag.value = false
-    }
+    const {
+      imageUploadState,
+      destroyImageUploadState,
+      onNewImgSet,
+      onNewDestroyed
+    } = useImageUpload()
 
     const isChanged = computed(
-      () => isStateChanged.value || imgData.value !== undefined
+      () => isStateChanged.value || imageUploadState.imgData !== undefined
     )
     const onUpdateClick = async () => {
       const promises = []
-      if (imgData.value !== undefined) {
-        promises.push(apis.changeUserIcon(detail.value.id, imgData.value))
+      if (imageUploadState.imgData !== undefined) {
+        promises.push(
+          apis.changeUserIcon(detail.value.id, imageUploadState.imgData)
+        )
       }
       if (isStateChanged.value) {
         const sendState = {
@@ -128,8 +128,7 @@ export default defineComponent({
       try {
         // TODO: loading
         await Promise.all(promises)
-        imgData.value = undefined
-        destroyFlag.value = true
+        destroyImageUploadState()
       } catch (e) {
         // TODO: error
       }
@@ -141,9 +140,9 @@ export default defineComponent({
       state,
       homeChannelState,
       isStateChanged,
-      onImgSet,
-      destroyFlag,
-      onDestroyed,
+      imageUploadState,
+      onNewImgSet,
+      onNewDestroyed,
       isChanged,
       onUpdateClick
     }
