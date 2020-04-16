7e93353e5b8df598cfe6cd2bc4a7e7c205c24954
sapphi-red <green@sapphi.red>
2020-04-16 21:35:54 +0900

    マークダウンの内部リンク close #241

---
+++ src/markdown-bridge.ts
@@ -0,0 +1,33 @@
+import store from '@/store'
+import { UserId } from '@/types/entity-ids'
+import router from './router'
+
+interface ExtendedWindow extends Window {
+  /**
+   * ユーザーモーダルを開く
+   * markdown本文に埋め込まれるリンク(`@user`)から呼び出す
+   * @param userId ユーザーID
+   */
+  openUserModal(userId: string): void
+
+  /**
+   * チャンネルを切り替える
+   * markdown本文に埋め込まれるリンク(`#channel`)から呼び出す
+   * @param channelPath チャンネルのパス(`#`は含まない、`/`区切り)
+   */
+  changeChannel(channelPath: string): void
+}
+declare const window: ExtendedWindow
+
+export const setupGlobalFuncs = () => {
+  window.openUserModal = (userId: UserId) => {
+    store.dispatch.ui.modal.pushModal({
+      type: 'user',
+      id: userId
+    })
+  }
+
+  window.changeChannel = (channelPath: string) => {
+    router.push(`/channels/${channelPath}`)
+  }
+}
