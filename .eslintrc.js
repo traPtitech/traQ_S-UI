9c29c092f50845bd847c316ed049a15dc86249d4
sapphi-red <green@sapphi.red>
2020-04-16 23:10:00 +0900

    eslint-plugin-unused-importsを追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,10 +11,14 @@ module.exports = {
     '@vue/prettier',
     '@vue/typescript'
   ],
+  plugins: ['unused-imports'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-fallthrough': 'error'
+    'no-fallthrough': 'error',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports-ts': 'warn',
+    'unused-imports/no-unused-vars-ts': 'off'
   },
   overrides: [
     {
