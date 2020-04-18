eeac814fff02567bb9d79c1ff8b801940387eb79
Yoya Mesaki <yoyamssk@gmail.com>
2020-04-18 10:21:48 +0900

    chores for mobile

--- src/components/Main/MainView/MainView.vue
+++ src/components/Main/MainView/MainView.vue
@@ -97,14 +97,11 @@ export default defineComponent({
 </script>
 
 <style lang="scss" module>
-$messagePadding: 32px;
-
 .container {
   display: flex;
   flex: 1 1;
   flex-direction: column;
   position: relative;
   height: 100%;
-  padding: 0 $messagePadding;
 }
 </style>
--- src/components/Main/MainView/MessageElement/MessageElement.vue
+++ src/components/Main/MainView/MessageElement/MessageElement.vue
@@ -6,6 +6,7 @@
     @mouseleave="onMouseLeave"
     ref="bodyRef"
     v-if="state.message"
+    :data-is-mobile="isMobile"
   >
     <user-icon
       :class="$style.userIcon"
@@ -51,6 +52,7 @@ import { makeStyles } from '@/lib/styles'
 import { transparentize } from '@/lib/util/color'
 import { MessageId } from '@/types/entity-ids'
 import useHover from '@/use/hover'
+import useIsMobile from '@/use/isMobile'
 import UserIcon from '@/components/UI/UserIcon.vue'
 import MessageHeader from './MessageHeader.vue'
 import MessageStampList from './MessageStampList.vue'
@@ -87,6 +89,7 @@ export default defineComponent({
   setup(props, context: SetupContext) {
     const { hoverState, onMouseEnter, onMouseLeave } = useHover(context)
     const bodyRef = ref<HTMLDivElement>(null)
+    const { isMobile } = useIsMobile()
     const state = reactive({
       message: computed(() => store.state.entities.messages[props.messageId]),
       content: computed(
@@ -106,12 +109,15 @@ export default defineComponent({
 
     const styles = useStyles(props, hoverState)
 
-    return { state, styles, onMouseEnter, onMouseLeave, bodyRef }
+    return { state, styles, onMouseEnter, onMouseLeave, bodyRef, isMobile }
   }
 })
 </script>
 
 <style lang="scss" module>
+$messagePadding: 32px;
+$messagePaddingMobile: 16px;
+
 .body {
   display: grid;
   grid-template:
@@ -122,8 +128,11 @@ export default defineComponent({
   grid-template-columns: 42px 1fr;
   width: 100%;
   min-width: 0;
-  padding: 8px 32px;
   overflow: hidden;
+  padding: 8px $messagePadding;
+  &[data-is-mobile='true'] {
+    padding: 8px $messagePaddingMobile;
+  }
 }
 
 .userIcon {
