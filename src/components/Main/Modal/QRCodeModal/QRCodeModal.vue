c0a194ae63fca470fa3daadfe04f1aaabb0c6ff3
Hosshii <sao_heath6147.wistre@icloud.com>
2020-04-22 20:49:45 +0900

    useStyles, useQrCode

--- src/components/Main/Modal/QRCodeModal/QRCodeModal.vue
+++ src/components/Main/Modal/QRCodeModal/QRCodeModal.vue
@@ -1,25 +1,72 @@
 <template>
   <modal-frame title="QR Code" icon-mdi icon-name="qrcode">
     <div
-      v-if="!state.err"
+      v-if="!err"
       :class="$style.qrCodeModalImage"
-      :style="qrImageStyle"
+      :style="styles.container"
     />
-    <modal-section v-else title="エラー" :description="state.err" />
+    <modal-section v-else title="エラー" :description="err" />
   </modal-frame>
 </template>
 
 <script lang="ts">
 import {
   defineComponent,
+  reactive,
   onMounted,
   onUnmounted,
-  reactive
+  toRefs,
+  Ref
 } from '@vue/composition-api'
-import apis from '@/lib/api'
+
 import { makeStyles } from '@/lib/styles'
 import ModalFrame from '../Common/ModalFrame.vue'
 import ModalSection from '../Common/ModalSection.vue'
+import apis from '@/lib/api'
+
+const useStyles = (url: Ref<string>) =>
+  reactive({
+    container: makeStyles(() => ({
+      backgroundImage: `url(${url.value})`
+    }))
+  })
+
+const useQrCode = () => {
+  const state = reactive({
+    url: '',
+    err: ''
+  })
+
+  onMounted(() => {
+    makeBlob()
+      .then(value => {
+        state.url = value
+      })
+      .catch(err => {
+        state.err = err
+      })
+  })
+
+  onUnmounted(() => {
+    URL.revokeObjectURL(state.url)
+  })
+
+  const makeBlob = async () => {
+    const response = await apis
+      .getMyQRCode(false, {
+        responseType: 'arraybuffer'
+      })
+      .catch(e => {
+        throw e
+      })
+
+    const blob = new Blob([response.data], { type: 'image/png' })
+
+    return URL.createObjectURL(blob)
+  }
+
+  return { ...toRefs(state) }
+}
 
 export default defineComponent({
   name: 'QrCodeModal',
@@ -28,44 +75,9 @@ export default defineComponent({
     ModalSection
   },
   setup(props) {
-    const state = reactive({
-      url: '',
-      err: ''
-    })
-
-    onMounted(() => {
-      makeBlob()
-        .then(value => {
-          state.url = value
-        })
-        .catch(err => {
-          state.err = err
-        })
-    })
-
-    onUnmounted(() => {
-      URL.revokeObjectURL(state.url)
-    })
-
-    const makeBlob = async () => {
-      const response = await apis
-        .getMyQRCode(false, {
-          responseType: 'arraybuffer'
-        })
-        .catch(e => {
-          throw e
-        })
-
-      const blob = new Blob([response.data], { type: 'image/png' })
-
-      return URL.createObjectURL(blob)
-    }
-
-    const qrImageStyle = makeStyles(() => ({
-      backgroundImage: `url(${state.url})`
-    }))
-
-    return { qrImageStyle, state }
+    const { url, err } = useQrCode()
+    const styles = useStyles(url)
+    return { styles, err }
   }
 })
 </script>
