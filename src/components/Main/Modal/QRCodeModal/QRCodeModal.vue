8fdfad4f4279355e9c1d2b6e2e7957fdb5271096
Hosshii <sao_heath6147.wistre@icloud.com>
2020-04-22 23:27:08 +0900

    fix

--- src/components/Main/Modal/QRCodeModal/QRCodeModal.vue
+++ src/components/Main/Modal/QRCodeModal/QRCodeModal.vue
@@ -1,11 +1,7 @@
 <template>
   <modal-frame title="QR Code" icon-mdi icon-name="qrcode">
-    <div
-      v-if="!err"
-      :class="$style.qrCodeModalImage"
-      :style="styles.container"
-    />
-    <modal-section v-else title="エラー" :description="err" />
+    <img v-if="!state.err" :src="state.url" :class="$style.qrCodeModalImage" />
+    <modal-section v-else title="エラー" :description="state.err" />
   </modal-frame>
 </template>
 
@@ -14,22 +10,11 @@ import {
   defineComponent,
   reactive,
   onMounted,
-  onUnmounted,
-  toRefs,
-  Ref
+  onUnmounted
 } from '@vue/composition-api'
-
-import { makeStyles } from '@/lib/styles'
 import ModalFrame from '../Common/ModalFrame.vue'
 import ModalSection from '../Common/ModalSection.vue'
 import apis from '@/lib/api'
-
-const useStyles = (url: Ref<string>) =>
-  reactive({
-    container: makeStyles(() => ({
-      backgroundImage: `url(${url.value})`
-    }))
-  })
 
 const useQrCode = () => {
   const state = reactive({
@@ -37,35 +22,27 @@ const useQrCode = () => {
     err: ''
   })
 
-  onMounted(() => {
-    makeBlob()
-      .then(value => {
-        state.url = value
-      })
-      .catch(err => {
-        state.err = err
-      })
+  onMounted(async () => {
+    try {
+      state.url = await makeURL()
+    } catch (err) {
+      state.err = err
+    }
   })
 
   onUnmounted(() => {
     URL.revokeObjectURL(state.url)
   })
 
-  const makeBlob = async () => {
-    const response = await apis
-      .getMyQRCode(false, {
-        responseType: 'arraybuffer'
-      })
-      .catch(e => {
-        throw e
-      })
+  const makeURL = async () => {
+    const { data } = await apis.getMyQRCode(false, {
+      responseType: 'blob'
+    })
 
-    const blob = new Blob([response.data], { type: 'image/png' })
-
-    return URL.createObjectURL(blob)
+    return URL.createObjectURL(data)
   }
 
-  return { ...toRefs(state) }
+  return { state }
 }
 
 export default defineComponent({
@@ -74,10 +51,10 @@ export default defineComponent({
     ModalFrame,
     ModalSection
   },
-  setup(props) {
-    const { url, err } = useQrCode()
-    const styles = useStyles(url)
-    return { styles, err }
+  setup() {
+    const { state } = useQrCode()
+
+    return { state }
   }
 })
 </script>
