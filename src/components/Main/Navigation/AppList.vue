f3e725d475dc9a1e756b84ac85a8f616bc0423f0
sapphi-red <green@sapphi.red>
2020-04-27 00:26:09 +0900

    サービス一覧を分離

--- src/components/Main/Navigation/AppList.vue
+++ src/components/Main/Navigation/AppList.vue
@@ -19,7 +19,6 @@
         :key="app.appName"
         :class="$style.item"
         :icon-name="app.iconName"
-        :icon-mdi="app.iconMdi"
         :label="app.label"
         :app-link="app.appLink"
       />
@@ -28,10 +27,11 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, reactive } from '@vue/composition-api'
+import { defineComponent, reactive } from '@vue/composition-api'
 import AppListItem from '@/components/Main/Navigation/AppListItem.vue'
 import { makeStyles } from '@/lib/styles'
 import CloseButton from '@/components/UI/CloseButton.vue'
+import config from '@/config'
 
 const useStyles = () =>
   reactive({
@@ -45,70 +45,7 @@ export default defineComponent({
   name: 'AppList',
   components: { AppListItem, CloseButton },
   setup(_, context) {
-    const apps = computed(
-      (): Array<{
-        label: string
-        appLink: string
-        iconName: string
-        iconMdi?: true
-      }> => [
-        {
-          label: 'Portal',
-          iconName: 'services/portal',
-          appLink: 'https://portal.trap.jp/'
-        },
-        {
-          label: 'Official Website',
-          iconName: 'services/traP',
-          appLink: 'https://trap.jp/'
-        },
-        {
-          label: 'Wiki',
-          iconName: 'services/growi',
-          appLink: 'https://wiki.trap.jp/'
-        },
-        {
-          label: 'Blog',
-          iconName: 'services/ghost',
-          appLink: 'https://blog-admin.trap.jp/'
-        },
-        {
-          label: 'Drive',
-          iconName: 'services/nextcloud',
-          appLink: 'https://drive.trap.jp/'
-        },
-        {
-          label: 'Git',
-          iconName: 'services/gitea',
-          appLink: 'https://git.trap.jp/'
-        },
-        {
-          label: 'Showcase',
-          iconName: 'services/showcase',
-          appLink: 'https://showcase.trapti.tech/'
-        },
-        {
-          label: 'HackMD',
-          iconName: 'services/hackmd',
-          appLink: 'https://md.trap.jp/'
-        },
-        {
-          label: 'anke-to',
-          iconName: 'services/anke-to',
-          appLink: 'https://anke-to.trap.jp/'
-        },
-        {
-          label: 'booQ',
-          iconName: 'services/booq',
-          appLink: 'https://booq.trap.jp/'
-        },
-        {
-          label: 'BOT Console',
-          iconName: 'services/bot-console',
-          appLink: 'https://bot-console.trap.jp/'
-        }
-      ]
-    )
+    const apps = config.services
 
     const close = () => {
       context.emit('close')
---
+++ src/config.ts
@@ -0,0 +1,59 @@
+export default {
+  services: [
+    {
+      label: 'Portal',
+      iconName: 'services/portal',
+      appLink: 'https://portal.trap.jp/'
+    },
+    {
+      label: 'Official Website',
+      iconName: 'services/traP',
+      appLink: 'https://trap.jp/'
+    },
+    {
+      label: 'Wiki',
+      iconName: 'services/growi',
+      appLink: 'https://wiki.trap.jp/'
+    },
+    {
+      label: 'Blog',
+      iconName: 'services/ghost',
+      appLink: 'https://blog-admin.trap.jp/'
+    },
+    {
+      label: 'Drive',
+      iconName: 'services/nextcloud',
+      appLink: 'https://drive.trap.jp/'
+    },
+    {
+      label: 'Git',
+      iconName: 'services/gitea',
+      appLink: 'https://git.trap.jp/'
+    },
+    {
+      label: 'Showcase',
+      iconName: 'services/showcase',
+      appLink: 'https://showcase.trapti.tech/'
+    },
+    {
+      label: 'HackMD',
+      iconName: 'services/hackmd',
+      appLink: 'https://md.trap.jp/'
+    },
+    {
+      label: 'anke-to',
+      iconName: 'services/anke-to',
+      appLink: 'https://anke-to.trap.jp/'
+    },
+    {
+      label: 'booQ',
+      iconName: 'services/booq',
+      appLink: 'https://booq.trap.jp/'
+    },
+    {
+      label: 'BOT Console',
+      iconName: 'services/bot-console',
+      appLink: 'https://bot-console.trap.jp/'
+    }
+  ]
+} as const
