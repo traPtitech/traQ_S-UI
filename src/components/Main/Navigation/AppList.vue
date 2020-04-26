1e5d686c79024e0acfdb6501f025f295a7edcb38
sapphi-red <green@sapphi.red>
2020-04-26 23:25:22 +0900

    サービスのアイコンの移動

--- src/components/Main/Navigation/AppList.vue
+++ src/components/Main/Navigation/AppList.vue
@@ -39,58 +39,58 @@ export default defineComponent({
       }> => [
         {
           label: 'Portal',
-          iconName: 'portal',
-          appLink: 'https://portal.trap.jp'
+          iconName: 'services/portal',
+          appLink: 'https://portal.trap.jp/'
         },
         {
           label: 'Official Website',
-          iconName: 'traQ',
-          appLink: 'https://trap.jp'
+          iconName: 'services/traP',
+          appLink: 'https://trap.jp/'
         },
         {
           label: 'Wiki',
-          iconName: 'growi',
-          appLink: 'https://wiki.trap.jp'
+          iconName: 'services/growi',
+          appLink: 'https://wiki.trap.jp/'
         },
         {
           label: 'Blog',
-          iconName: 'ghost',
-          appLink: 'https://blog-admin.trap.jp'
+          iconName: 'services/ghost',
+          appLink: 'https://blog-admin.trap.jp/'
         },
         {
           label: 'Drive',
-          iconName: 'next-cloud',
-          appLink: 'https://drive.trap.jp'
+          iconName: 'services/nextcloud',
+          appLink: 'https://drive.trap.jp/'
         },
         {
           label: 'Git',
-          iconName: 'gitea',
-          appLink: 'https://git.trap.jp'
+          iconName: 'services/gitea',
+          appLink: 'https://git.trap.jp/'
         },
         {
           label: 'Showcase',
-          iconName: 'showcase',
-          appLink: 'https://showcase.trapti.tech'
+          iconName: 'services/showcase',
+          appLink: 'https://showcase.trapti.tech/'
         },
         {
           label: 'HackMD',
-          iconName: 'hack-md',
-          appLink: 'https://md.trap.jp'
+          iconName: 'services/hackmd',
+          appLink: 'https://md.trap.jp/'
         },
         {
           label: 'anke-to',
-          iconName: 'anke-to',
-          appLink: 'https://anke-to.trap.jp'
+          iconName: 'services/anke-to',
+          appLink: 'https://anke-to.trap.jp/'
         },
         {
           label: 'booQ',
-          iconName: 'booq',
-          appLink: 'https://booq.trap.jp'
+          iconName: 'services/booq',
+          appLink: 'https://booq.trap.jp/'
         },
         {
           label: 'BOT Console',
-          iconName: 'bot-console',
-          appLink: 'https://bot-console.trap.jp'
+          iconName: 'services/bot-console',
+          appLink: 'https://bot-console.trap.jp/'
         }
       ]
     )
