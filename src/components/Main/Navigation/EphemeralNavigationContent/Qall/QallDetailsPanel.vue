dc9d3dccd419ed72d2e3782c015652c9077deba8
spaspa <yoyamssk@gmail.com>
2020-04-27 17:55:40 +0900

    impl slider ui

--- src/components/Main/Navigation/EphemeralNavigationContent/Qall/QallDetailsPanel.vue
+++ src/components/Main/Navigation/EphemeralNavigationContent/Qall/QallDetailsPanel.vue
@@ -1,36 +1,60 @@
 <template>
-  <div :class="$style.container" :style="styles.container">
-    <slider v-model="state.po" />
+  <div :class="$style.container">
+    <qall-details-panel-user-volume-slider
+      :class="$style.slider"
+      v-if="me"
+      :key="me"
+      :user-id="me"
+    />
+    <qall-details-panel-user-volume-slider
+      :class="$style.slider"
+      v-for="id in users"
+      :key="id"
+      :user-id="id"
+    />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed, reactive } from '@vue/composition-api'
-import Slider from '@/components/UI/Slider.vue'
+import { defineComponent, computed } from '@vue/composition-api'
+
 import store from '@/store'
+import QallDetailsPanelUserVolumeSlider from './QallDetailsPanelUserVolumeSlider.vue'
 
 export default defineComponent({
   name: 'QallDetailsPanel',
   components: {
-    Slider
+    QallDetailsPanelUserVolumeSlider
   },
   setup() {
-    const state = reactive({
-      po: 0
-    })
+    const me = computed(() => store.state.domain.me.detail?.id)
     const users = computed(() => {
       const currentSession = store.getters.app.rtc.qallSession
       if (!currentSession) {
         return []
       }
-      return store.state.app.rtc.sessionUsersMap[currentSession.sessionId] ?? []
+      return (
+        store.state.app.rtc.sessionUsersMap[currentSession.sessionId]?.filter(
+          id => id !== me.value
+        ) ?? []
+      )
     })
-    return { state }
+    return { users, me }
   }
 })
 </script>
 
 <style lang="scss" module>
 .container {
+  padding: 12px;
+}
+.slider {
+  margin: 8px 0;
+  &:first-child {
+    margin-top: 0;
+  }
+  &:last-child {
+    margin-bottom: 0;
+  }
 }
 </style>
---
+++ src/components/Main/Navigation/EphemeralNavigationContent/Qall/QallDetailsPanelUserVolumeSlider.vue
@@ -0,0 +1,44 @@
+<template>
+  <div :class="$style.container">
+    <user-icon :class="$style.icon" :user-id="userId" :size="28" />
+    <div :class="$style.slider">
+      <slider :value="50" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from '@vue/composition-api'
+import Slider from '@/components/UI/Slider.vue'
+import UserIcon from '@/components/UI/UserIcon.vue'
+import { UserId } from '@/types/entity-ids'
+
+export default defineComponent({
+  name: 'QallDetailsPanelUserVolumeSlider',
+  components: {
+    Slider,
+    UserIcon
+  },
+  props: {
+    userId: { type: String as PropType<UserId>, required: true }
+  },
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss" module>
+.container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.icon {
+  margin-right: 12px;
+  flex-shrink: 0;
+}
+.slider {
+  width: 100%;
+}
+</style>
