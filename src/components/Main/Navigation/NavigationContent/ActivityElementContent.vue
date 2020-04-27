d78c31397c7cc52b30c95eafca82c98e98486f32
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2020-04-27 14:14:57 +0900

    アクティビティカードのメッセージレンダリング

--- src/components/Main/Navigation/NavigationContent/ActivityElementContent.vue
+++ src/components/Main/Navigation/NavigationContent/ActivityElementContent.vue
@@ -1,13 +1,17 @@
 <template>
   <div :class="$style.container" :style="styles.container">
     <!-- TODO: Markdownパース対応 -->
-    {{ content }}
+    <!-- {{ content }} -->
+    <div :class="['markdown-body', $style.content]" v-html="state.content" />
+    <!-- <div :class="$style.content" v-html="state.content" /> -->
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from '@vue/composition-api'
+import { defineComponent, reactive, computed } from '@vue/composition-api'
 import { makeStyles } from '@/lib/styles'
+import { render } from '@/lib/markdown'
+import { embeddingExtractor } from '@/lib/embeddingExtractor'
 
 export default defineComponent({
   name: 'ActivityElementContent',
@@ -23,8 +27,16 @@ export default defineComponent({
         color: theme.ui.primary
       }))
     })
+    const state = reactive({
+      content: computed(() => {
+        const extracted = embeddingExtractor(props.content)
+        const renderedContent = render(extracted.text)
+        return renderedContent
+      })
+    })
     return {
-      styles
+      styles,
+      state
     }
   }
 })
@@ -34,5 +46,27 @@ export default defineComponent({
 .container {
   font-size: 1rem;
   word-break: break-all;
+  width: 100%;
+}
+.content {
+  a {
+    pointer-events: none;
+    color: currentColor;
+  }
+  pre {
+    background-color: initial;
+    padding: 0;
+    overflow: initial;
+  }
+  code {
+    background-color: initial;
+    padding: 0;
+    font-size: initial;
+  }
+  span {
+    color: currentColor;
+    word-break: break-all;
+    white-space: pre-wrap;
+  }
 }
 </style>
