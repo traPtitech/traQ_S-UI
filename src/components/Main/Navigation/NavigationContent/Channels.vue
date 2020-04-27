223d38ed3d1b90aaabcbfd040606f34429827140
Hosshii <sao_heath6147.wistre@icloud.com>
2020-04-27 13:34:09 +0900

    test

--- src/components/Main/Navigation/NavigationContent/Channels.vue
+++ src/components/Main/Navigation/NavigationContent/Channels.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     チャンネル検索
+    <!-- {{ staredChannel }} -->
     <filter-input
       :on-secondary="true"
       :text="channelListFilterState.query"
@@ -21,10 +22,15 @@
         />
       </div>
     </div>
+    <button @click="test(tree)">button</button>
     <channel-list
       v-show="channelListFilterState.query.length <= 0"
-      :channels="topLevelChannels"
+      :channels="tree.children"
     />
+    <!-- <channel-list
+      v-show="channelListFilterState.query.length <= 0"
+      :channels="topLevelChannels"
+    /> -->
   </div>
 </template>
 
@@ -35,6 +41,8 @@ import ChannelList from '@/components/Main/Navigation/ChannelList/ChannelList.vu
 import useTextFilter from '@/use/textFilter'
 import FilterInput from '@/components/UI/FilterInput.vue'
 import ChannelFilteredElement from '../ChannelList/ChannelFilteredElement.vue'
+import { constructTree } from '../../../../store/domain/channelTree/actions'
+import { ChannelTreeNode } from '../../../../store/domain/channelTree/state'
 
 const useChannelListFilter = () => {
   const channels = computed(() => Object.values(store.state.entities.channels))
@@ -45,6 +53,39 @@ const useChannelListFilter = () => {
   }
 }
 
+const useStaredChannel = () => {
+  const staredChannelId = computed(() =>
+    Object.keys(store.state.domain.me.staredChannelSet).map(
+      v => store.state.entities.channels[v]
+    )
+  )
+
+  const buildPath = (parentid: string | null, name: string): string => {
+    if (parentid != null) {
+      const parentCh = store.state.entities.channels[parentid]
+      name = parentCh.name[0] + '/' + name
+      if (parentCh.parentId) {
+        return buildPath(parentCh.parentId, name)
+      }
+    }
+    return name
+  }
+  const tree = computed(() => {
+    return {
+      children: staredChannelId.value.map(ch => {
+        const tree = constructTree(ch, store.state.entities.channels)
+        if (tree != undefined) {
+          const path = buildPath(ch.parentId, ch.name)
+          tree.name = path
+        }
+        return tree
+      }) as ChannelTreeNode[]
+    }
+  })
+
+  return { tree }
+}
+
 export default defineComponent({
   name: 'Channels',
   components: {
@@ -53,18 +94,55 @@ export default defineComponent({
     ChannelFilteredElement
   },
   setup() {
-    const topLevelChannels = computed(
-      () => store.state.domain.channelTree.channelTree.children ?? []
-    )
+    const topLevelChannels = computed(() => {
+      return store.state.domain.channelTree.channelTree.children ?? []
+    })
+    // const staredChannel = computed(() =>
+    //   Object.keys(store.state.domain.me.staredChannelSet).map(
+    //     v => store.state.entities.channels[v]
+    //   )
+    // )
+    // const tree = computed(() => {
+    //   return {
+    //     children: staredChannel.value.map(ch => {
+    //       const tree = constructTree(ch, store.state.entities.channels)
+    //       if (tree != undefined) {
+    //         const path = buildPath(ch.parentId, ch.name)
+    //         tree.name = path
+    //       }
+    //       return tree
+    //     }) as ChannelTreeNode[]
+    //   }
+    // })
+
+    // const buildPath = (parentid: string | null, name: string): string => {
+    //   if (parentid != null) {
+    //     const parentCh = store.state.entities.channels[parentid]
+    //     name = parentCh.name[0] + '/' + name
+    //     if (parentCh.parentId) {
+    //       return buildPath(parentCh.parentId, name)
+    //     }
+    //   }
+    //   return name
+    // }
+
+    const test = (test: any) => {
+      console.log(test)
+    }
+
     const { channelListFilterState, setQuery } = useChannelListFilter()
     const channelId = computed(
       () => store.state.domain.messagesView.currentChannelId
     )
+    const { tree } = useStaredChannel()
     return {
       topLevelChannels,
       channelListFilterState,
       setQuery,
-      channelId
+      channelId,
+      // staredChannel,
+      tree,
+      test
     }
   }
 })
