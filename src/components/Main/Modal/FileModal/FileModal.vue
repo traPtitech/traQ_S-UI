9488448320e3c99d700cb9754d48894af6416c39
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-04-30 02:24:11 +0900

    fix

--- src/components/Main/Modal/FileModal/FileModal.vue
+++ src/components/Main/Modal/FileModal/FileModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div v-click-outside="onClickOutSide">
     <file-modal-image v-if="fileType === 'image'" :file-id="fileMeta.id" />
     <file-modal-video v-else-if="fileType === 'video'" :file-id="fileMeta.id" />
     <file-modal-audio v-else-if="fileType === 'audio'" :file-id="fileMeta.id" />
@@ -10,6 +10,7 @@
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api'
 import useFileMeta from '@/use/fileMeta'
+import store from '@/store'
 import FileModalImage from '@/components/Main/Modal/FileModal/FileModalImage.vue'
 import FileModalFile from '@/components/Main/Modal/FileModal/FileModalFile.vue'
 import FileModalVideo from '@/components/Main/Modal/FileModal/FileModalVideo.vue'
@@ -31,7 +32,8 @@ export default defineComponent({
   },
   setup(props, context) {
     const { fileMeta, fileType } = useFileMeta(props, context)
-    return { fileMeta, fileType }
+    const onClickOutSide = () => store.dispatch.ui.modal.clearModal()
+    return { fileMeta, fileType, onClickOutSide }
   }
 })
 </script>
