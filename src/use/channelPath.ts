b1620ff4e13209efe8b2983357c907d87a23f69e
takashi <t.k.mk3.110@gmail.com>
2020-04-29 23:24:41 +0900

    fix dm channel path

--- src/use/channelPath.ts
+++ src/use/channelPath.ts
@@ -48,13 +48,23 @@ const useChannelPath = () => {
   const channelIdToPath = (id: ChannelId): string[] =>
     channelIdToSimpleChannelPath(id).map(c => c.name)
 
-  const channelIdToPathString = (id: ChannelId, hashed = false): string =>
-    (hashed ? '#' : '') + channelIdToPath(id).join('/')
+  const dmChannelIdToPathString = (id: ChannelId, hashed = false): string =>
+    (hashed ? '@' : '') +
+      store.state.entities.users[store.state.entities.dmChannels[id].userId]
+        ?.name ?? ''
+
+  const channelIdToPathString = (id: ChannelId, hashed = false): string => {
+    if (id in store.state.entities.dmChannels)
+      return dmChannelIdToPathString(id, hashed)
+    return (hashed ? '#' : '') + channelIdToPath(id).join('/')
+  }
 
   const channelIdToShortPathString = (
     id: ChannelId,
     hashed = false
   ): string => {
+    if (id in store.state.entities.dmChannels)
+      return dmChannelIdToPathString(id, hashed)
     const channels = channelIdToPath(id)
     const formattedChannels = channels.slice(0, -1).map(c => c[0])
     formattedChannels.push(channels.pop() ?? '')
