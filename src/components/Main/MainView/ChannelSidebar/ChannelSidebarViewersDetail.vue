7137319c282ee4f01c72b98258fd6867b6d3a1a7
spaspa <yoyamssk@gmail.com>
2020-04-30 17:19:56 +0900

    show name for viewers

--- src/components/Main/MainView/ChannelSidebar/ChannelSidebarViewersDetail.vue
+++ src/components/Main/MainView/ChannelSidebar/ChannelSidebarViewersDetail.vue
@@ -1,24 +1,53 @@
 <template>
   <sidebar-content-container clickable title="閲覧者" @click="closeDetail">
-    <user-icon v-for="id in viewerIds" :key="id" :user-id="id" :size="40" />
+    <div v-for="user in users" :key="user.id" :class="$style.item">
+      <user-icon :user-id="user.id" :size="32" />
+      <div :class="$style.userName">{{ user.displayName }}</div>
+    </div>
   </sidebar-content-container>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from '@vue/composition-api'
+import { defineComponent, PropType, computed } from '@vue/composition-api'
 import UserIcon from '@/components/UI/UserIcon.vue'
 import { UserId } from '@/types/entity-ids'
 import SidebarContentContainer from '@/components/Main/MainView/MainViewSidebar/SidebarContentContainer.vue'
+import store from '@/store'
+import { User } from '@traptitech/traq'
 
 export default defineComponent({
   name: 'ChannelSidebarViewerDetail',
   components: { UserIcon, SidebarContentContainer },
   props: { viewerIds: { type: Array as PropType<UserId[]>, default: [] } },
-  setup(_, context) {
+  setup(props, context) {
     const closeDetail = () => {
       context.emit('close')
     }
-    return { closeDetail }
+    const users = computed(
+      () =>
+        props.viewerIds
+          .map(id => store.state.entities.users[id])
+          .filter(u => !!u) as User[]
+    )
+    return { closeDetail, users }
   }
 })
 </script>
+
+<style lang="scss" module>
+.item {
+  display: flex;
+  align-items: center;
+  margin: 4px 0;
+  &:first-child {
+    margin-top: 0;
+  }
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+.userName {
+  margin-left: 8px;
+  font-weight: bold;
+}
+</style>
