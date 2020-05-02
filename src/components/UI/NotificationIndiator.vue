65c32b1d8708057ce17b1cef0a072bb30c3b56d4
sapphi-red <green@sapphi.red>
2020-05-02 14:29:13 +0900

    fix NotificationIndicator's borderColor is not reactive

--- src/components/UI/NotificationIndiator.vue
+++ src/components/UI/NotificationIndiator.vue
@@ -5,15 +5,12 @@
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api'
 import store from '@/store'
-import { makeStyles } from '../../lib/styles'
+import { makeStyles } from '@/lib/styles'
 
 export default defineComponent({
   name: 'NotificationIndicator',
   props: {
-    color: {
-      type: String,
-      default: store.getters.app.themeSettings.currentTheme.background.secondary
-    },
+    color: String,
     hasBorder: {
       type: Boolean,
       default: false
@@ -22,7 +19,9 @@ export default defineComponent({
   setup(props) {
     const style = makeStyles(theme => ({
       background: theme.accent.notification,
-      borderColor: props.color,
+      borderColor:
+        props.color ??
+        store.getters.app.themeSettings.currentTheme.background.secondary,
       borderWidth: props.hasBorder ? '2px' : '0'
     }))
     return { style }
