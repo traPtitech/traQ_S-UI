d16ae5133808ab14968fb929e82aaa268ef270f5
spaspa <yoyamssk@gmail.com>
2020-05-11 00:31:01 +0900

    add mainview

--- src/components/Main/MainView/MainViewComponentSelector.vue
+++ src/components/Main/MainView/MainViewComponentSelector.vue
@@ -1,15 +1,14 @@
 <template>
   <channel-view
-    :class="$style.messagesView"
     v-if="viewInfo.type === 'channel' || viewInfo.type === 'dm'"
     :channel-id="viewInfo.channelId"
     :entry-message-id="viewInfo.entryMessageId"
   />
   <clips-view
-    :class="$style.messagesView"
     v-else-if="viewInfo.type === 'clips'"
     :clip-folder-id="viewInfo.clipFolderId"
   />
+  <qall-view v-else-if="viewInfo.type === 'qall'" />
   <div :class="$style.none" v-else></div>
 </template>
 
@@ -18,10 +17,11 @@ import { defineComponent, PropType } from '@vue/composition-api'
 import { ViewInformation } from '@/store/ui/mainView/state'
 import ChannelView from '@/components/Main/MainView/ChannelView/ChannelView.vue'
 import ClipsView from '@/components/Main/MainView/ClipsView/ClipsView.vue'
+import QallView from '@/components/Main/MainView/QallView/QallView.vue'
 
 export default defineComponent({
   name: 'MainViewComponentSelector',
-  components: { ChannelView, ClipsView },
+  components: { ChannelView, ClipsView, QallView },
   props: {
     viewInfo: {
       type: Object as PropType<ViewInformation>,
--- src/components/Main/MainView/QallView/QallView.vue
+++ src/components/Main/MainView/QallView/QallView.vue
@@ -1,33 +1,36 @@
 <template>
-  <div :class="$style.Block">
-    <h1 :class="$style.Header">
-      Qall View
-    </h1>
+  <div :class="$style.container">
+    <video ref="$remoteVideoRef" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent, ref, watchEffect } from '@vue/composition-api'
+import store from '@/store'
 
 export default defineComponent({
   name: 'QallView',
   props: {},
   setup() {
-    return {}
+    const $remoteVideoRef = ref<HTMLVideoElement>(null)
+    watchEffect(() => {
+      if ($remoteVideoRef.value) {
+        $remoteVideoRef.value.srcObject =
+          Object.values(store.state.app.rtc.remoteVideoStreamMap)[0] ?? null
+      }
+    })
+    return { $remoteVideoRef }
   }
 })
 </script>
 
 <style lang="scss" module>
-.Block {
-  color: green;
-}
-
-.Header {
-  font: {
-    size: 30px;
-    weight: bold;
-  }
-  color: green;
+.container {
+  @include background-common-black;
+  @include color-ui-primary;
+  display: flex;
+  flex-direction: row;
+  position: relative;
+  height: 100%;
 }
 </style>
