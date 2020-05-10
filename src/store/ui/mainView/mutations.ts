db0b3d5961a3b964bfad3268f18896ab96143344
spaspa <yoyamssk@gmail.com>
2020-05-11 00:56:23 +0900

    change layout on start screen sharing

--- src/store/ui/mainView/mutations.ts
+++ src/store/ui/mainView/mutations.ts
@@ -2,16 +2,19 @@ import { defineMutations } from 'direct-vuex'
 import { S, LayoutType, ViewInformation, MainViewComponentState } from './state'
 
 export const mutations = defineMutations<S>()({
-  setLayout(state: S, layout: LayoutType) {
+  setLayout(state, layout: LayoutType) {
     state.layout = layout
   },
-  setMainViewComponentState(state: S, componentState: MainViewComponentState) {
+  setMainViewComponentState(state, componentState: MainViewComponentState) {
     state.currentMainViewComponentState = componentState
   },
-  setPrimaryView(state: S, view: ViewInformation) {
+  setPrimaryView(state, view: ViewInformation) {
     state.primaryView = view
   },
-  setSecondaryView(state: S, view: ViewInformation) {
-    state.primaryView = view
+  setSecondaryView(state, view: ViewInformation) {
+    state.secondaryView = view
+  },
+  unsetSecondaryView(state) {
+    state.secondaryView = undefined
   }
 })
