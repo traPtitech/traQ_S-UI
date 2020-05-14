66f5b0c5dd8e208275a0317aa6441c9ad21d2a61
sapphi-red <green@sapphi.red>
2020-05-14 13:16:38 +0900

    一番下のメッセージのツールメニューが画面外にはみ出てた fix #445

--- src/components/Main/MainView/MessageElement/MessageToolsMenuContainer.vue
+++ src/components/Main/MainView/MessageElement/MessageToolsMenuContainer.vue
@@ -1,23 +1,33 @@
 <template>
   <portal v-show="state.isPopupMenuShown" :to="targetPortalName">
-    <message-tools-menu
-      v-if="state.isPopupMenuShown"
-      :style="styles.toolsMenu"
-      :class="$style.toolsMenu"
-      :message-id="state.messageId"
-      v-click-outside="closePopupMenu"
-    />
+    <div ref="menuContainerRef">
+      <message-tools-menu
+        v-if="state.isPopupMenuShown"
+        :style="styles.toolsMenu"
+        :class="$style.toolsMenu"
+        :message-id="state.messageId"
+        v-click-outside="closePopupMenu"
+      />
+    </div>
   </portal>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, computed } from '@vue/composition-api'
+import {
+  defineComponent,
+  reactive,
+  computed,
+  ref,
+  Ref,
+  watch,
+  SetupContext
+} from '@vue/composition-api'
 import store from '@/store'
 import { makeStyles } from '@/lib/styles'
 import MessageToolsMenu from './MessageToolsMenu.vue'
 import { targetPortalName } from '@/views/Main.vue'
 
-const useStampPicker = () => {
+const useMenu = () => {
   const state = reactive({
     messageId: computed(() => store.state.ui.messageContextMenu.target),
     isPopupMenuShown: computed(() => store.getters.ui.messageContextMenu.isShow)
@@ -25,16 +35,36 @@ const useStampPicker = () => {
   return { state }
 }
 
-const useStyles = (state: { isPopupMenuShown: boolean }) =>
+const useMenuHeight = (
+  context: SetupContext,
+  state: { isPopupMenuShown: boolean }
+) => {
+  const height = ref(0)
+  const menuContainerRef = ref<HTMLDivElement>(null)
+  watch(
+    () => state.isPopupMenuShown,
+    async newVal => {
+      if (!newVal) return
+      await context.root.$nextTick()
+      const $menu = menuContainerRef.value?.firstElementChild
+      height.value = $menu?.clientHeight ?? 0
+    }
+  )
+  return { height, menuContainerRef }
+}
+
+const useStyles = (state: { isPopupMenuShown: boolean }, height: Ref<number>) =>
   reactive({
-    toolsMenu: makeStyles(theme => ({
-      top: state.isPopupMenuShown
-        ? `${store.state.ui.messageContextMenu.position.y}px`
-        : '',
-      left: state.isPopupMenuShown
-        ? `${store.state.ui.messageContextMenu.position.x}px`
-        : ''
-    }))
+    toolsMenu: makeStyles(theme => {
+      if (!state.isPopupMenuShown) return {}
+      const margin = 20
+      return {
+        top: `min(calc(100vh - ${height.value + margin}px), ${
+          store.state.ui.messageContextMenu.position.y
+        }px)`,
+        left: `${store.state.ui.messageContextMenu.position.x}px`
+      }
+    })
   })
 
 export default defineComponent({
@@ -42,13 +72,22 @@ export default defineComponent({
   components: {
     MessageToolsMenu
   },
-  setup() {
-    const { state } = useStampPicker()
-    const styles = useStyles(state)
+  setup(_, context) {
+    const { state } = useMenu()
+    const { height, menuContainerRef } = useMenuHeight(context, state)
+
+    const styles = useStyles(state, height)
     const closePopupMenu = () => {
       store.dispatch.ui.messageContextMenu.closeMessageContextMenu()
     }
-    return { state, styles, targetPortalName, closePopupMenu }
+    return {
+      state,
+      height,
+      menuContainerRef,
+      styles,
+      targetPortalName,
+      closePopupMenu
+    }
   }
 })
 </script>
