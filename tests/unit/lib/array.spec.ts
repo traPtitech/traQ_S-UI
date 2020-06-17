485c7a6e733248bcf0e66c106fe0c07ecf43ca92
sapphi-red <green@sapphi.red>
2020-06-17 19:11:53 +0900

    expectedとactualが入れ替わってた

--- tests/unit/lib/array.spec.ts
+++ tests/unit/lib/array.spec.ts
@@ -4,24 +4,24 @@ describe('getFullMatchedAndMatched', () => {
   it('can get fullMatched', () => {
     const expected = { fullMatched: ['aaaaa'], matched: [] }
     const actual = getFullMatchedAndMatched(arr, 'aaaaa')
-    expect(expected).toEqual(actual)
+    expect(actual).toEqual(expected)
   })
 
   it('can get matched (1)', () => {
     const expected = { fullMatched: [], matched: ['ddddd'] }
     const actual = getFullMatchedAndMatched(arr, 'dd')
-    expect(expected).toEqual(actual)
+    expect(actual).toEqual(expected)
   })
   it('can get matched (2)', () => {
     const expected = { fullMatched: [], matched: ['abcde', 'cde', 'cdecd'] }
     const actual = getFullMatchedAndMatched(arr, 'cd')
-    expect(expected).toEqual(actual)
+    expect(actual).toEqual(expected)
   })
 
   it('can get fullMatched and matched', () => {
     const expected = { fullMatched: ['aaa'], matched: ['aaaaa', 'AAAAb'] }
     const actual = getFullMatchedAndMatched(arr, 'aaa')
-    expect(expected).toEqual(actual)
+    expect(actual).toEqual(expected)
   })
 })
 
