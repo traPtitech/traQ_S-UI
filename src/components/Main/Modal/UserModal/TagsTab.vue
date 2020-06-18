34656f169b32187fc3a6a194d8a03cc1410f9014
sapphi-red <green@sapphi.red>
2020-06-18 09:37:57 +0900

    ユーザーモーダルのタグタブのリファクタ

--- src/components/Main/Modal/UserModal/TagsTab.vue
+++ src/components/Main/Modal/UserModal/TagsTab.vue
@@ -3,19 +3,13 @@
     <template v-if="detail === undefined">Now loading...</template>
     <template v-else>
       <ul :class="$style.list">
-        <li v-for="tag in tags" :key="tag.tagId" :class="$style.tag">
-          <div @click="onTagClick(tag.tagId)" :class="$style.content">
-            <icon name="tag" mdi :class="$style.icon" :size="20" />
-            <div :class="$style.text">
-              {{ tag.tag }}
-            </div>
-          </div>
-          <tags-tab-edit
-            :tag-id="tag.tagId"
-            :user-id="userId"
-            :class="$style.edit"
-          />
-        </li>
+        <tags-tab-tag
+          v-for="tag in tags"
+          :class="$style.tag"
+          :key="tag.tagId"
+          :tag="tag"
+          :is-mine="isMine"
+        />
       </ul>
       <tags-tab-add :user-id="userId" />
     </template>
@@ -26,10 +20,8 @@
 import { defineComponent, computed, PropType } from '@vue/composition-api'
 import store from '@/store'
 import { UserDetail } from '@traptitech/traq'
-import { TagId } from '@/types/entity-ids'
-import Icon from '@/components/UI/Icon.vue'
+import TagsTabTag from '@/components/Main/Modal/UserModal/TagsTabTag.vue'
 import TagsTabAdd from '@/components/Main/Modal/UserModal/TagsTabAdd.vue'
-import TagsTabEdit from '@/components/Main/Modal/UserModal/TagsTabEdit.vue'
 
 export default defineComponent({
   name: 'TagsTab',
@@ -38,25 +30,20 @@ export default defineComponent({
   },
   setup(props) {
     const userId = computed(() => props.detail?.id)
+    const isMine = computed(
+      () => userId.value === store.state.domain.me.detail?.id
+    )
     const tags = computed(() => props.detail?.tags ?? [])
-
-    const onTagClick = (id: TagId) => {
-      store.dispatch.ui.modal.pushModal({
-        type: 'tag',
-        id
-      })
-    }
 
     return {
       userId,
-      tags,
-      onTagClick
+      isMine,
+      tags
     }
   },
   components: {
-    Icon,
-    TagsTabAdd,
-    TagsTabEdit
+    TagsTabTag,
+    TagsTabAdd
   }
 })
 </script>
@@ -76,34 +63,9 @@ export default defineComponent({
 
 .tag {
   margin: 16px 8px;
-  cursor: pointer;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
   &:first-child {
     // ナビゲーションと頭を揃える
     margin-top: 8px;
   }
-}
-
-.content {
-  display: flex;
-  align-items: center;
-  min-width: 0;
-}
-
-.icon {
-  margin-right: 8px;
-  flex-shrink: 0;
-}
-
-.text {
-  overflow-wrap: break-word;
-  min-width: 0;
-}
-.edit {
-  margin-left: 8px;
-  width: 44px;
-  flex-shrink: 0;
 }
 </style>
