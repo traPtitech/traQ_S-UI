18f2d298ea9a89cb8ecf39983bb1b984fe2da86c
sapphi-red <green@sapphi.red>
2020-06-25 21:51:49 +0900

    起動ローディング表示 close #1118

--- src/components/Main/MainView/MainViewComponentSelector.vue
+++ src/components/Main/MainView/MainViewComponentSelector.vue
@@ -1,16 +1,14 @@
 <template>
   <channel-view
-    :class="$style.messagesView"
     v-if="viewInfo.type === 'channel' || viewInfo.type === 'dm'"
     :channel-id="viewInfo.channelId"
     :entry-message-id="viewInfo.entryMessageId"
   />
   <clips-view
-    :class="$style.messagesView"
     v-else-if="viewInfo.type === 'clips'"
     :clip-folder-id="viewInfo.clipFolderId"
   />
-  <div :class="$style.none" v-else></div>
+  <null-view v-else />
 </template>
 
 <script lang="ts">
@@ -18,10 +16,11 @@ import { defineComponent, PropType } from '@vue/composition-api'
 import { ViewInformation } from '@/store/ui/mainView/state'
 import ChannelView from '@/components/Main/MainView/ChannelView/ChannelView.vue'
 import ClipsView from '@/components/Main/MainView/ClipsView/ClipsView.vue'
+import NullView from '@/components/Main/MainView/NullView/NullView.vue'
 
 export default defineComponent({
   name: 'MainViewComponentSelector',
-  components: { ChannelView, ClipsView },
+  components: { ChannelView, ClipsView, NullView },
   props: {
     viewInfo: {
       type: Object as PropType<ViewInformation>,
@@ -33,9 +32,3 @@ export default defineComponent({
   }
 })
 </script>
-
-<style lang="scss" module>
-.none {
-  display: none;
-}
-</style>
---
+++ src/components/Main/MainView/NullView/NullView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div :class="$style.container">
+    <icon :class="$style.logo" name="traQ" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+import Icon from '@/components/UI/Icon.vue'
+
+export default defineComponent({
+  name: 'NullView',
+  components: {
+    Icon
+  },
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss" module>
+.container {
+  @include background-primary;
+  @include color-ui-primary;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.logo {
+  @include color-accent-primary;
+  height: min(20vmin, 100px);
+  width: min(20vmin, 100px);
+}
+</style>
