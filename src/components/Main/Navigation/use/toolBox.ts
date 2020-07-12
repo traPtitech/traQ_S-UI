56d47863971b97fa873aa53c4d4fea13a14df73e
harsh <mizuki.k.aa@m.titech.ac.jp>
2020-07-12 22:18:49 +0900

    ツールボックスからテーマアイコンを消去

--- src/components/Main/Navigation/use/toolBox.ts
+++ src/components/Main/Navigation/use/toolBox.ts
@@ -5,33 +5,6 @@ import usePopupMenu from '../../MainView/ChannelView/use/popupMenu'
 const useToolBox = () => {
   const { isPopupMenuShown, closePopupMenu, togglePopupMenu } = usePopupMenu()
 
-  const themeIcon = computed(() => {
-    const { type } = store.state.app.themeSettings
-    switch (type) {
-      case 'light':
-        return 'crescent-outline'
-      case 'dark':
-        return 'crescent'
-      case 'custom':
-        return 'brightness-6'
-      default: {
-        const invalid: never = type
-        // eslint-disable-next-line no-console
-        console.warn(`Invalid theme type: ${invalid}`)
-
-        return 'crescent'
-      }
-    }
-  })
-  const isMdi = computed(() => {
-    return store.state.app.themeSettings.type === 'custom' ? true : undefined
-  })
-
-  const isThemeClickDisabled = computed(
-    () => store.state.app.themeSettings.type === 'custom'
-  )
-
-  const toggleTheme = () => store.dispatch.app.themeSettings.toggleTheme()
   const openQrCodeModal = () =>
     store.dispatch.ui.modal.pushModal({ type: 'qrcode' })
   const openSettingsModal = () =>
@@ -49,13 +22,6 @@ const useToolBox = () => {
         iconName: 'apps',
         iconMdi: true,
         onClick: togglePopupMenu
-      },
-      // theme
-      {
-        iconName: themeIcon.value,
-        iconMdi: isMdi.value,
-        disabled: isThemeClickDisabled.value,
-        onClick: toggleTheme
       },
       // qr
       {
