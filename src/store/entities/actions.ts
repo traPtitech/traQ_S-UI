55522e65fd73b626b57b946c8fa8c285ce25d4cb
sapphi-red <green@sapphi.red>
2020-07-30 00:14:26 +0900

    unicodeのスタンプのキャッシュ

--- src/store/entities/actions.ts
+++ src/store/entities/actions.ts
@@ -13,6 +13,7 @@ import {
   UserId
 } from '@/types/entity-ids'
 import { ActionContext } from 'vuex'
+import { getUnicodeStamps, setUnicodeStamps } from '@/lib/stampCache'
 
 // TODO: リクエストパラメータの型置き場
 interface BaseGetMessagesParams {
@@ -83,8 +84,15 @@ export const actions = defineActions({
   },
   async fetchStamps(context) {
     const { commit } = entitiesActionContext(context)
-    const res = await apis.getStamps()
-    commit.setStamps(reduceToRecord(res.data, 'id'))
+
+    const unicodeStamps = await getUnicodeStamps()
+    const res = await apis.getStamps(!unicodeStamps)
+    if (!unicodeStamps) {
+      setUnicodeStamps(res.data.filter(stamp => stamp.isUnicode))
+    }
+
+    const stamps = unicodeStamps ? [...unicodeStamps, ...res.data] : res.data
+    commit.setStamps(reduceToRecord(stamps, 'id'))
   },
   async fetchStampPalettes(context) {
     const { commit } = entitiesActionContext(context)
