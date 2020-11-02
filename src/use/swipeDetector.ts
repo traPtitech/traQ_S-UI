424d73427e9baa56f564056d7807deed87bd7aa0
sapphi-red <green@sapphi.red>
2020-11-03 00:53:30 +0900

    サイドバーをスパイプで開けないことがあった

--- src/use/swipeDetector.ts
+++ src/use/swipeDetector.ts
@@ -27,6 +27,11 @@ const isHorizontalScrollable = (e: Readonly<TouchEvent>) => {
 
   let inspectingTarget = e.target as HTMLElement
 
+  const overflowX = getComputedStyle(inspectingTarget).getPropertyValue(
+    'overflow-x'
+  )
+  if (overflowX !== 'scroll') return false
+
   while (inspectingTarget.parentElement !== e.currentTarget) {
     const scrollWidth = inspectingTarget.scrollWidth
     const clientWidth = inspectingTarget.clientWidth
