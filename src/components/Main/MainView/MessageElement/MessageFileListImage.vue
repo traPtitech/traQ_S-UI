255dbef00ba5bd6afdfa0c8ccb22b1c3c334c3ad
sapphi-red <green@sapphi.red>
2020-11-27 21:24:06 +0900

    intrinsicsizeについてのコメントを追加

--- src/components/Main/MainView/MessageElement/MessageFileListImage.vue
+++ src/components/Main/MainView/MessageElement/MessageFileListImage.vue
@@ -1,5 +1,9 @@
 <template>
   <router-link :to="fileLink" v-if="isLarge" :class="$style.largeContainer">
+    <!--
+      height, widthはlayout shift対策
+      https://www.mizdra.net/entry/2020/05/31/192613
+    -->
     <img
       draggable="false"
       :alt="fileMeta.name"
@@ -9,6 +13,9 @@
     />
   </router-link>
   <router-link v-else :to="fileLink" :class="$style.container">
+    <!--
+      CSSで固定値指定なのでheight, widthはつけない
+    -->
     <img draggable="false" :alt="fileMeta.name" :src="fileThumbnailPath" />
   </router-link>
 </template>
