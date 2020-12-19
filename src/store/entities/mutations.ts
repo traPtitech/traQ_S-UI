35c556a1bc2e41979b758a2d0abfddbaff3f23af
sapphi-red <green@sapphi.red>
2020-12-19 23:23:16 +0900

    mutationsでの型推論の利用

--- src/store/entities/mutations.ts
+++ src/store/entities/mutations.ts
@@ -20,36 +20,36 @@ import { S } from './state'
 import _store from '@/_store'
 
 export const mutations = defineMutations<S>()({
-  setUser(state: S, user: User) {
+  setUser(state, user: User) {
     state.usersMap.set(user.id, user)
   },
-  setUsersMap(state: S, usersMap: Map<UserId, User>) {
+  setUsersMap(state, usersMap: Map<UserId, User>) {
     state.usersMap = usersMap
     state.usersMapFetched = true
   },
-  deleteUser(state: S, userId: UserId) {
+  deleteUser(state, userId: UserId) {
     state.usersMap.delete(userId)
   },
 
-  setUserGroup(state: S, userGroup: UserGroup) {
+  setUserGroup(state, userGroup: UserGroup) {
     state.userGroupsMap.set(userGroup.id, userGroup)
   },
-  setUserGroupsMap(state: S, userGroupsMap: Map<UserGroupId, UserGroup>) {
+  setUserGroupsMap(state, userGroupsMap: Map<UserGroupId, UserGroup>) {
     state.userGroupsMap = userGroupsMap
     state.userGroupsMapFetched = true
   },
-  deleteUserGroup(state: S, userGroupId: UserGroupId) {
+  deleteUserGroup(state, userGroupId: UserGroupId) {
     state.userGroupsMap.delete(userGroupId)
   },
 
-  setChannel(state: S, channel: Channel) {
+  setChannel(state, channel: Channel) {
     state.channelsMap.set(channel.id, channel)
   },
-  setDmChannel(state: S, dmChannel: DMChannel) {
+  setDmChannel(state, dmChannel: DMChannel) {
     state.dmChannelsMap.set(dmChannel.id, dmChannel)
   },
   setBothChannelsMap(
-    state: S,
+    state,
     [channelsMap, dmChannelsMap]: [
       Map<ChannelId, Channel>,
       Map<ChannelId, DMChannel>
@@ -62,51 +62,51 @@ export const mutations = defineMutations<S>()({
     // TODO: eventを使うようにする
     _store.dispatch.domain.channelTree.constructAllTrees()
   },
-  deleteChannel(state: S, channelId: ChannelId) {
+  deleteChannel(state, channelId: ChannelId) {
     state.channelsMap.delete(channelId)
   },
 
-  setClipFolder(state: S, clipFolder: ClipFolder) {
+  setClipFolder(state, clipFolder: ClipFolder) {
     state.clipFoldersMap.set(clipFolder.id, clipFolder)
   },
-  setClipFoldersMap(state: S, clipFoldersMap: Map<ClipFolderId, ClipFolder>) {
+  setClipFoldersMap(state, clipFoldersMap: Map<ClipFolderId, ClipFolder>) {
     state.clipFoldersMap = clipFoldersMap
   },
-  deleteClipFolder(state: S, clipFolderId: ClipFolderId) {
+  deleteClipFolder(state, clipFolderId: ClipFolderId) {
     state.clipFoldersMap.delete(clipFolderId)
   },
 
-  setStamp(state: S, stamp: Stamp) {
+  setStamp(state, stamp: Stamp) {
     state.stampsMap.set(stamp.id, stamp)
 
     // TODO: eventを使うようにする
     _store.dispatch.domain.stampCategory.constructStampCategories()
   },
-  setStampsMap(state: S, stampsMap: Map<StampId, Stamp>) {
+  setStampsMap(state, stampsMap: Map<StampId, Stamp>) {
     state.stampsMap = stampsMap
     state.stampsMapFetched = true
 
     // TODO: eventを使うようにする
     _store.dispatch.domain.stampCategory.constructStampCategories()
   },
-  deleteStamp(state: S, stampId: StampId) {
+  deleteStamp(state, stampId: StampId) {
     state.stampsMap.delete(stampId)
 
     // TODO: eventを使うようにする
     _store.dispatch.domain.stampCategory.constructStampCategories()
   },
 
-  setStampPalette(state: S, stampPalette: StampPalette) {
+  setStampPalette(state, stampPalette: StampPalette) {
     state.stampPalettesMap.set(stampPalette.id, stampPalette)
   },
   setStampPalettesMap(
-    state: S,
+    state,
     stampPalettesMap: Map<StampPaletteId, StampPalette>
   ) {
     state.stampPalettesMap = stampPalettesMap
     state.stampPalettesMapFetched = true
   },
-  deleteStampPalette(state: S, stampPaletteId: StampPaletteId) {
+  deleteStampPalette(state, stampPaletteId: StampPaletteId) {
     state.stampPalettesMap.delete(stampPaletteId)
   }
 })
