c70cfe820b6c980e5b843a6a93e233ee863a7983
FUJISHIGE TEMMA <tenma.x0@gmail.com>
2021-02-03 17:56:07 +0900

    :zap:ユーザーアイコンとオンラインインジケーターの表示

--- src/components/Main/MainView/MessageInput/DropdownSuggester.vue
+++ src/components/Main/MainView/MessageInput/DropdownSuggester.vue
@@ -5,22 +5,40 @@
         [$style.item]: true,
         [$style.selected]: index === currentIndex
       }"
-      v-for="(candidate, index) in candidates"
+      v-for="(candidate, index) in candidatesWithId"
       :key="candidate"
       @click="onClick(index)"
     >
-      <div :class="$style.name">
-        {{ candidate }}
-      </div>
+      <template v-if="candidate.isUser">
+        <user-icon :user-id="candidate.userId" :size="24" />
+        <online-indicator
+          :class="$style.indicator"
+          :user-id="candidate.userId"
+          :size="8"
+        />
+        <div :class="$style.name">
+          {{ candidate.word }}
+        </div>
+      </template>
+      <template v-else :class="$style.name">
+        {{ candidate.word }}
+      </template>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { defineComponent, computed, PropType } from 'vue'
+import store from '@/store'
+import UserIcon from '@/components/UI/UserIcon.vue'
+import OnlineIndicator from '@/components/UI/OnlineIndicator.vue'
 
 export default defineComponent({
   name: 'DropdownSuggester',
+  components: {
+    UserIcon,
+    OnlineIndicator
+  },
   props: {
     candidates: {
       type: Array as PropType<string[]>,
@@ -35,11 +53,37 @@ export default defineComponent({
     select: (index: number) => true
   },
   setup(props, context) {
+    const candidatesWithId = computed(() =>
+      props.candidates.map(word => {
+        if ([...word][0] !== '@')
+          return {
+            isUser: false,
+            word,
+            userId: ''
+          }
+        const userId = store.getters.entities.userByName(
+          [...word].slice(1).join('')
+        )?.id
+        if (!userId)
+          return {
+            isUser: false,
+            word,
+            userId: ''
+          }
+        return {
+          isUser: true,
+          word,
+          userId
+        }
+      })
+    )
+
     const onClick = (index: number) => {
       context.emit('select', index)
     }
 
     return {
+      candidatesWithId,
       onClick
     }
   }
@@ -61,6 +105,7 @@ export default defineComponent({
   z-index: $z-index-word-suggester;
 }
 .item {
+  display: flex;
   padding: 4px;
   cursor: pointer;
   &.selected,
@@ -70,7 +115,11 @@ export default defineComponent({
     font-weight: bold;
   }
 }
+.indicator {
+  flex-shrink: 0;
+}
 .name {
+  margin-left: 4px;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
