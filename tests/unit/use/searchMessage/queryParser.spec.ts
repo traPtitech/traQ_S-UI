792aec2a4e1319c73b9abe4cd1e92bfc8c004374
spaspa <spa@spaspa.dev>
2021-03-13 15:20:13 +0900

    パーサーのインターフェイスを少し変更

--- tests/unit/use/searchMessage/queryParser.spec.ts
+++ tests/unit/use/searchMessage/queryParser.spec.ts
@@ -1,10 +1,12 @@
-import { parseQueryToObject } from '@/use/searchMessage/queryParser'
+import useQueryParer from '@/use/searchMessage/queryParser'
 import store from '@/store'
 
 const mockChannelId = 'channel-id'
 const mockChannelName = 'general'
 const mockUserId = 'user-id'
 const mockUserName = 'user'
+
+const { parseQuery, toSearchMessageParam } = useQueryParer()
 
 beforeAll(async () => {
   store.commit.entities.setUser({
@@ -28,65 +30,106 @@ beforeAll(async () => {
   await store.dispatch.domain.channelTree.constructAllTrees()
 })
 
-describe('parseQueryToObject', () => {
+describe('parseQuery', () => {
   it('can parse query without filter', () => {
     const query = 'lorem       ipsum'
-    const parsed = parseQueryToObject(query)
+    const parsed = parseQuery(query)
     expect(parsed.word).toEqual('lorem ipsum')
   })
   it('can parse query with date-filter', () => {
     const query = 'lorem ipsum after:2021-01-23'
-    const parsed = parseQueryToObject(query)
+    const parsed = parseQuery(query)
     expect(parsed.word).toEqual(`lorem ipsum`)
     expect(parsed.after).toEqual('2021-01-23T00:00:00.000Z')
   })
   it('can parse query with in-filter (prefix: `in:`)', () => {
     const query = `lorem ipsum in:${mockChannelName}`
-    const parsed = parseQueryToObject(query)
+    const parsed = parseQuery(query)
     expect(parsed.word).toEqual(`lorem ipsum`)
     expect(parsed.in).toEqual(mockChannelId)
   })
   it('can parse query with in-filter (prefix: `#`)', () => {
     const query = `lorem ipsum #${mockChannelName}`
-    const parsed = parseQueryToObject(query)
+    const parsed = parseQuery(query)
     expect(parsed.word).toEqual(`lorem ipsum`)
     expect(parsed.in).toEqual(mockChannelId)
   })
   it('can parse query with user-filter without @', () => {
     const query = `lorem ipsum from:${mockUserName}`
-    const parsed = parseQueryToObject(query)
+    const parsed = parseQuery(query)
     expect(parsed.word).toEqual(`lorem ipsum`)
     expect(parsed.from).toEqual(mockUserId)
   })
   it('can parse query with user-filter with @', () => {
     const query = `lorem ipsum from:@${mockUserName}`
-    const parsed = parseQueryToObject(query)
+    const parsed = parseQuery(query)
     expect(parsed.word).toEqual(`lorem ipsum`)
     expect(parsed.from).toEqual(mockUserId)
   })
   it('can parse query with flag-filter', () => {
     const query = 'lorem has:image ipsum'
-    const parsed = parseQueryToObject(query)
+    const parsed = parseQuery(query)
     expect(parsed.word).toEqual(`lorem ipsum`)
     expect(parsed.hasImage).toEqual(true)
   })
   it('can parse query with negated flag-filter (not-style)', () => {
     const query = 'lorem ipsum not:bot'
-    const parsed = parseQueryToObject(query)
+    const parsed = parseQuery(query)
     expect(parsed.word).toEqual(`lorem ipsum`)
     expect(parsed.bot).toEqual(false)
   })
   it('can parse query with negated flag-filter (negation-style)', () => {
     const query = 'lorem ipsum -is:bot'
-    const parsed = parseQueryToObject(query)
+    const parsed = parseQuery(query)
     expect(parsed.word).toEqual(`lorem ipsum`)
     expect(parsed.bot).toEqual(false)
   })
   it('can parse query with wrong valued-filter', () => {
     // @phantomさんは存在しないのでこのクエリはwordに入る
     const query = 'lorem ipsum from:@phantom'
-    const parsed = parseQueryToObject(query)
+    const parsed = parseQuery(query)
     expect(parsed.word).toEqual(`lorem ipsum from:@phantom`)
     expect(parsed.from).toEqual(undefined)
+  })
+})
+
+describe('toSearchMessageParam', () => {
+  it('can convert query object to api param array', () => {
+    const query = {
+      word: 'lorem ipsum',
+      after: '2021-01-23T00:00:00.000Z',
+      before: '2021-01-23T00:00:00.000Z',
+      in: mockChannelId,
+      to: mockUserId,
+      from: mockUserId,
+      citation: undefined,
+      bot: false,
+      hasUrl: true,
+      hasAttachments: true,
+      hasImage: false
+    }
+    const options = {
+      limit: 20,
+      sort: 'createdAt' as const
+    }
+    const params = toSearchMessageParam(query, options)
+    expect(params).toEqual([
+      'lorem ipsum',
+      '2021-01-23T00:00:00.000Z',
+      '2021-01-23T00:00:00.000Z',
+      mockChannelId,
+      mockUserId,
+      mockUserId,
+      undefined,
+      false,
+      true,
+      true,
+      false,
+      undefined,
+      undefined,
+      20,
+      undefined,
+      'createdAt'
+    ])
   })
 })
