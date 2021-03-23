9e55a2b49899de0ef5adfbe285a3dd4d95bec1b9
sapphi-red <green@sapphi.red>
2021-03-24 00:41:09 +0900

    EcoModeの分離

---
+++ src/components/Settings/BrowserTab/EcoMode.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div :class="$style.eco">
+      <h3 :class="$style.header">省エネモード</h3>
+      <toggle v-model="value" :class="$style.toggle" />
+    </div>
+    <div :class="$style.content">
+      <p>
+        省エネモードがONの場合、スタンプエフェクトのアニメーションを表示しません
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Toggle from '@/components/UI/Toggle.vue'
+import useModelSyncer from '@/use/modelSyncer'
+
+export default defineComponent({
+  name: 'EcoMode',
+  components: {
+    Toggle
+  },
+  props: {
+    modelValue: {
+      type: Boolean,
+      required: true
+    }
+  },
+  setup(props, context) {
+    const value = useModelSyncer(props, context)
+    return { value }
+  }
+})
+</script>
+
+<style lang="scss" module>
+.header {
+  margin-bottom: 8px;
+}
+.content {
+  margin-left: 12px;
+}
+.eco {
+  display: flex;
+  align-items: center;
+  margin-bottom: 8px;
+  h3 {
+    margin: 0;
+  }
+  .toggle {
+    margin-left: 12px;
+  }
+}
+</style>
--- src/views/Settings/BrowserTab.vue
+++ src/views/Settings/BrowserTab.vue
@@ -11,18 +11,8 @@
       v-model:sendWithModifierKey="state.sendWithModifierKey"
       v-model:modifierKey="state.modifierKey"
     />
-    <div :class="$style.element">
-      <div :class="$style.echo">
-        <h3 :class="$style.header">省エネモード</h3>
-        <toggle v-model="state.ecoMode" :class="$style.toggle" />
-      </div>
-      <div :class="$style.content">
-        <p>
-          省エネモードがONの場合、スタンプエフェクトのアニメーションを表示しません
-        </p>
-      </div>
-    </div>
-    <caches />
+    <eco-mode :class="$style.element" v-model="state.ecoMode" />
+    <caches :class="$style.element" />
   </section>
 </template>
 
@@ -30,29 +20,28 @@
 import { defineComponent, computed } from 'vue'
 import store from '@/store'
 import useSyncedState from '@/components/Settings/use/syncedState'
-import Toggle from '@/components/UI/Toggle.vue'
 import Notification from '@/components/Settings/BrowserTab/Notification.vue'
 import Caches from '@/components/Settings/BrowserTab/Caches.vue'
 import OpenMode from '@/components/Settings/BrowserTab/OpenMode.vue'
 import SendKey from '@/components/Settings/BrowserTab/SendKey.vue'
+import EcoMode from '@/components/Settings/BrowserTab/EcoMode.vue'
 
 export default defineComponent({
   name: 'BrowserTab',
+  components: {
+    Notification,
+    OpenMode,
+    SendKey,
+    EcoMode,
+    Caches
+  },
   setup() {
     const browserSettings = computed(() => store.state.app.browserSettings)
     const { state } = useSyncedState(
       browserSettings,
       store.commit.app.browserSettings.set
     )
-
     return { state }
-  },
-  components: {
-    Notification,
-    OpenMode,
-    SendKey,
-    Toggle,
-    Caches
   }
 })
 </script>
@@ -60,22 +49,5 @@ export default defineComponent({
 <style lang="scss" module>
 .element {
   margin: 24px 0;
-}
-.header {
-  margin-bottom: 8px;
-}
-.content {
-  margin-left: 12px;
-}
-.echo {
-  display: flex;
-  align-items: center;
-  margin-bottom: 8px;
-  h3 {
-    margin: 0;
-  }
-  .toggle {
-    margin-left: 12px;
-  }
 }
 </style>
