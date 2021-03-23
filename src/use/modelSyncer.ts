a9bc3a729cea06fff52da4463cb3912b461f5c80
sapphi-red <green@sapphi.red>
2021-03-24 00:36:10 +0900

    SendKeyの分離

--- src/use/modelSyncer.ts
+++ src/use/modelSyncer.ts
@@ -1,4 +1,4 @@
-import { computed, SetupContext } from 'vue'
+import { computed, ComputedRef, SetupContext } from 'vue'
 
 const useModelSyncer = <T>(
   props: { modelValue: T },
@@ -18,3 +18,25 @@ const useModelSyncer = <T>(
 }
 
 export default useModelSyncer
+
+export const useModelObjectSyncer = <P, K extends keyof P>(
+  props: P,
+  key = 'modelValue' as K,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  emit: (name: string, ...args: any[]) => void
+): { [KK in keyof P[K]]: ComputedRef<P[K][KK]> } => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const computedObject: any = {}
+  for (const k of Object.keys(props[key])) {
+    computedObject[k] = computed({
+      get() {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        return (props[key] as any)[k]
+      },
+      set(v) {
+        emit(`update:${key}`, { ...props[key], [k]: v })
+      }
+    })
+  }
+  return computedObject
+}
--- src/views/Settings/BrowserTab.vue
+++ src/views/Settings/BrowserTab.vue
@@ -6,49 +6,11 @@
       v-model:open-mode="state.openMode"
       v-model:open-channel-name="state.openChannelName"
     />
-    <div :class="$style.element">
-      <h3 :class="$style.header">メッセージ送信スタイル</h3>
-      <div :class="$style.content">
-        <form-radio
-          label="修飾キー+Enterで送信 / Enterで改行"
-          input-value="modifier"
-          v-model="state.sendWithModifierKey"
-          :class="$style.sendWithModifierKey"
-        />
-        <form-radio
-          label="Enterで送信 / 修飾キー+Enterで改行"
-          input-value="none"
-          v-model="state.sendWithModifierKey"
-          :class="$style.sendWithModifierKey"
-        />
-        <div :class="$style.key">
-          <p>修飾キーとして利用するキー</p>
-          <div>
-            <form-checkbox
-              :label="getModifierKeyName('shift')"
-              v-model="state.modifierKey.shift"
-              :class="$style.keyCheckbox"
-            />
-            <form-checkbox
-              :label="getModifierKeyName('alt')"
-              v-model="state.modifierKey.alt"
-              :class="$style.keyCheckbox"
-            />
-            <form-checkbox
-              :label="getModifierKeyName('ctrl')"
-              v-model="state.modifierKey.ctrl"
-              :class="$style.keyCheckbox"
-            />
-            <form-checkbox
-              v-if="macFlag"
-              :label="getModifierKeyName('macCtrl')"
-              v-model="state.modifierKey.macCtrl"
-              :class="$style.keyCheckbox"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
+    <send-key
+      :class="$style.element"
+      v-model:sendWithModifierKey="state.sendWithModifierKey"
+      v-model:modifierKey="state.modifierKey"
+    />
     <div :class="$style.element">
       <div :class="$style.echo">
         <h3 :class="$style.header">省エネモード</h3>
@@ -67,57 +29,28 @@
 <script lang="ts">
 import { defineComponent, computed } from 'vue'
 import store from '@/store'
-import { isMac } from '@/lib/util/browser'
-import { SendKeys } from '@/store/app/browserSettings'
 import useSyncedState from '@/components/Settings/use/syncedState'
-import FormRadio from '@/components/UI/FormRadio.vue'
-import FormCheckbox from '@/components/UI/FormCheckbox.vue'
 import Toggle from '@/components/UI/Toggle.vue'
 import Notification from '@/components/Settings/BrowserTab/Notification.vue'
 import Caches from '@/components/Settings/BrowserTab/Caches.vue'
 import OpenMode from '@/components/Settings/BrowserTab/OpenMode.vue'
-
-const windowsModifierKeyTable: Record<keyof SendKeys, string> = {
-  alt: 'Alt',
-  ctrl: 'Ctrl',
-  shift: 'Shift',
-  macCtrl: ''
-}
-const macModifierKeyTable: Record<keyof SendKeys, string> = {
-  alt: '⌥(Option)',
-  ctrl: '⌘(Command)',
-  shift: 'Shift',
-  macCtrl: 'Ctrl'
-}
+import SendKey from '@/components/Settings/BrowserTab/SendKey.vue'
 
 export default defineComponent({
   name: 'BrowserTab',
   setup() {
-    // 起動時チャンネルの選択に必要
-    store.dispatch.entities.fetchChannels()
-
     const browserSettings = computed(() => store.state.app.browserSettings)
     const { state } = useSyncedState(
       browserSettings,
       store.commit.app.browserSettings.set
     )
 
-    const macFlag = isMac()
-    const getModifierKeyName = (key: keyof SendKeys) => {
-      return macFlag ? macModifierKeyTable[key] : windowsModifierKeyTable[key]
-    }
-
-    return {
-      state,
-      macFlag,
-      getModifierKeyName
-    }
+    return { state }
   },
   components: {
     Notification,
     OpenMode,
-    FormRadio,
-    FormCheckbox,
+    SendKey,
     Toggle,
     Caches
   }
@@ -131,24 +64,8 @@ export default defineComponent({
 .header {
   margin-bottom: 8px;
 }
-.sendWithModifierKey {
-  display: inline-block;
-  margin-right: 12px;
-}
-.keyCheckbox {
-  margin-right: 12px;
-}
 .content {
   margin-left: 12px;
-}
-.key {
-  display: flex;
-  margin-top: 16px;
-  flex-flow: row wrap;
-  align-items: center;
-  > div {
-    margin-left: 24px;
-  }
 }
 .echo {
   display: flex;
