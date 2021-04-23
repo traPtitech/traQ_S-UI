29d785417b6e87ef6f1e7b67aa8343c4bb6f321a
sapphi-red <green@sapphi.red>
2021-04-23 22:13:50 +0900

    traQアイコン長押しで戻る/進むを表示するように

--- src/components/Main/MainView/MainViewHeader/MainViewHeader.vue
+++ src/components/Main/MainView/MainViewHeader/MainViewHeader.vue
@@ -1,8 +1,23 @@
 <template>
   <header :class="$style.container">
     <div :class="$style.headerContainer">
-      <button v-if="isMobile" :class="$style.navigationButton" @click="openNav">
-        <icon name="traQ" />
+      <button
+        v-if="isMobile"
+        :class="$style.navigationButton"
+        @pointerdown="onPointerDown"
+        @pointerup="onPointerUpOrLeave"
+      >
+        <icon name="traQ" :size="28" />
+        <div v-show="isLongClicking" :class="$style.popupNavigator">
+          <div :class="$style.popupNavigatorItem" @pointerup="movePrev">
+            <icon name="arrow-left" mdi :class="$style.icon" />
+            戻る
+          </div>
+          <div :class="$style.popupNavigatorItem" @pointerup="moveNext">
+            <icon name="arrow-right" mdi :class="$style.icon" />
+            進む
+          </div>
+        </div>
       </button>
       <h2 :class="$style.headerBody">
         <slot name="header" />
@@ -13,10 +28,44 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import Icon from '@/components/UI/Icon.vue'
 import useIsMobile from '@/use/isMobile'
 import useNavigationController from '@/use/navigationController'
+import { useRouter } from 'vue-router'
+
+/**
+ * タッチにも対応している
+ */
+const useIsLongClicking = (onMouseClick: () => void) => {
+  const isLongClicking = ref(false)
+  let timer = 0
+
+  const onPointerDown = (e: PointerEvent) => {
+    timer = window.setTimeout(() => {
+      isLongClicking.value = true
+    }, 100)
+  }
+  const onPointerUpOrLeave = (e: PointerEvent) => {
+    window.clearTimeout(timer)
+    if (!isLongClicking.value && e.type === 'pointerup') {
+      onMouseClick()
+    }
+    isLongClicking.value = false
+  }
+  return { isLongClicking, onPointerDown, onPointerUpOrLeave }
+}
+
+const useNavigator = () => {
+  const router = useRouter()
+  const movePrev = () => {
+    router.back()
+  }
+  const moveNext = () => {
+    router.forward()
+  }
+  return { movePrev, moveNext }
+}
 
 export default defineComponent({
   name: 'MainViewHeader',
@@ -26,7 +75,18 @@ export default defineComponent({
   setup() {
     const { isMobile } = useIsMobile()
     const { openNav } = useNavigationController()
+    const {
+      isLongClicking,
+      onPointerDown,
+      onPointerUpOrLeave
+    } = useIsLongClicking(openNav)
+    const { movePrev, moveNext } = useNavigator()
     return {
+      isLongClicking,
+      onPointerDown,
+      onPointerUpOrLeave,
+      movePrev,
+      moveNext,
       isMobile,
       openNav
     }
@@ -61,9 +121,33 @@ $headerHeight: 80px;
 }
 .navigationButton {
   @include color-ui-primary;
+  position: relative;
   display: flex;
+  height: 36px;
+  width: 36px;
+  justify-content: center;
   align-items: center;
   margin-right: 8px;
+}
+.popupNavigator {
+  @include background-primary;
+  @include drop-shadow-default;
+  position: absolute;
+  top: -8px;
+  left: -8px;
+  border-radius: 4px;
+  user-select: none;
+  white-space: nowrap;
+  z-index: $z-index-header-popup-navigator;
+}
+.popupNavigatorItem {
+  padding: 12px 20px;
+  &:hover {
+    @include background-secondary;
+  }
+}
+.icon {
+  vertical-align: middle;
 }
 .tools {
   flex-shrink: 0;
