5a461e0f20354b41c13894777384b4466269d43b
sapphi-red <green@sapphi.red>
2021-05-05 02:13:03 +0900

    Avoid global pollution

--- public/defaultTheme.js
+++ public/defaultTheme.js
@@ -1,60 +1,61 @@
-//@ts-check
-
-/**
- * @type {import('../src/types/theme').Theme}
- */
-const lightTheme = {
-  accent: {
-    primary: '#005BAC',
-    notification: '#F2994A',
-    online: '#28F0E4',
-    error: '#F26451',
-    focus: '#005BACC0'
-  },
-  background: {
-    primary: '#FFFFFF',
-    secondary: '#F0F2F5',
-    tertiary: '#E2E5E9',
-    secondarySub: '#FBFCFE'
-  },
-  ui: {
-    primary: '#49535B',
-    secondary: '#6B7D8A',
-    tertiary: '#CED6DB'
-  },
-  text: {
-    primary: '#333333',
-    secondary: '#79797A'
+// @ts-check
+;(() => {
+  /**
+   * @type {import('../src/types/theme').Theme}
+   */
+  const lightTheme = {
+    accent: {
+      primary: '#005BAC',
+      notification: '#F2994A',
+      online: '#28F0E4',
+      error: '#F26451',
+      focus: '#005BACC0'
+    },
+    background: {
+      primary: '#FFFFFF',
+      secondary: '#F0F2F5',
+      tertiary: '#E2E5E9',
+      secondarySub: '#FBFCFE'
+    },
+    ui: {
+      primary: '#49535B',
+      secondary: '#6B7D8A',
+      tertiary: '#CED6DB'
+    },
+    text: {
+      primary: '#333333',
+      secondary: '#79797A'
+    }
   }
-}
 
-/**
- * @type {import('../src/types/theme').Theme}
- */
-const darkTheme = {
-  accent: {
-    primary: '#4899F9',
-    notification: '#F2994A',
-    online: '#28F0E4',
-    error: '#F26451',
-    focus: '#4899F9C0'
-  },
-  background: {
-    primary: '#242B33',
-    secondary: '#1E262E',
-    tertiary: '#1A242D',
-    secondarySub: '#1E262F'
-  },
-  ui: {
-    primary: '#F2F5F8',
-    secondary: '#C7D0D9',
-    tertiary: '#8795A3'
-  },
-  text: {
-    primary: '#FFFFFF',
-    secondary: '#BAC2C9'
+  /**
+   * @type {import('../src/types/theme').Theme}
+   */
+  const darkTheme = {
+    accent: {
+      primary: '#4899F9',
+      notification: '#F2994A',
+      online: '#28F0E4',
+      error: '#F26451',
+      focus: '#4899F9C0'
+    },
+    background: {
+      primary: '#242B33',
+      secondary: '#1E262E',
+      tertiary: '#1A242D',
+      secondarySub: '#1E262F'
+    },
+    ui: {
+      primary: '#F2F5F8',
+      secondary: '#C7D0D9',
+      tertiary: '#8795A3'
+    },
+    text: {
+      primary: '#FFFFFF',
+      secondary: '#BAC2C9'
+    }
   }
-}
 
-window.defaultLightTheme = lightTheme
-window.defaultDarkTheme = darkTheme
+  window.defaultLightTheme = lightTheme
+  window.defaultDarkTheme = darkTheme
+})()
