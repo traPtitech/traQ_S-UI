6b33ffbd7d9e04abcc0b69e9b9bf955299bc18ca
sapphi-red <green@sapphi.red>
2021-05-05 17:53:39 +0900

    thumbnailsを利用するように

--- src/use/fileThumbnail.ts
+++ src/use/fileThumbnail.ts
@@ -1,25 +1,24 @@
 import { computed } from 'vue'
 import { buildFileThumbnailPath } from '@/lib/apis'
 import useFileMeta from './fileMeta'
+import { ThumbnailType } from '@traptitech/traq'
 
 const useFileThumbnail = (props: { fileId: string }) => {
   const { fileMeta, fileLink, fileRawPath } = useFileMeta(props)
+  const fileThumbnail = computed(() =>
+    fileMeta.value?.thumbnails.find(t => t.type === ThumbnailType.Image)
+  )
+
   const name = computed(() => fileMeta.value?.name ?? '')
   const fileThumbnailPath = computed(() =>
-    fileMeta.value && fileMeta.value.thumbnail !== null
+    fileMeta.value && fileThumbnail.value
       ? buildFileThumbnailPath(fileMeta.value.id)
       : fileRawPath.value
   )
-  const fileThumbnailSize = computed(() => {
-    const thumbnail =
-      fileMeta.value && fileMeta.value.thumbnail !== null
-        ? fileMeta.value.thumbnail
-        : undefined
-    return {
-      height: thumbnail?.height,
-      width: thumbnail?.width
-    }
-  })
+  const fileThumbnailSize = computed(() => ({
+    height: fileThumbnail.value?.height,
+    width: fileThumbnail.value?.width
+  }))
   const isAnimatedImage = computed(
     () => fileMeta.value?.isAnimatedImage ?? false
   )
