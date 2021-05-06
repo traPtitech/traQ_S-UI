0d50e79be8db0d26a2c402259c8e2bad85e15b2f
sapphi-red <green@sapphi.red>
2021-05-06 16:03:20 +0900

    useAudioのaudioをrefに

--- src/components/UI/use/audio.ts
+++ src/components/UI/use/audio.ts
@@ -1,38 +1,43 @@
-import {
-  Ref,
-  ref,
-  computed,
-  watch,
-  readonly,
-  unref,
-  isRef,
-  onMounted
-} from 'vue'
+import { Ref, ref, computed, watch, readonly, shallowRef } from 'vue'
 import usePictureInPicture from './pictureInPicture'
 import { FileInfo } from '@traptitech/traq'
 
 const toFinite = (n: number, def: number) => (Number.isFinite(n) ? n : def)
 
-const useIsPlaying = (audio: HTMLAudioElement) => {
+const useIsPlaying = (audio: Ref<HTMLAudioElement | undefined>) => {
   const isPlayingNative = ref(false)
   const wasUnsupportedType = ref(false)
 
-  audio.addEventListener('play', () => {
+  const onPlay = () => {
     isPlayingNative.value = true
-  })
-  audio.addEventListener('pause', () => {
+  }
+  const onStop = () => {
     isPlayingNative.value = false
-  })
-  audio.addEventListener('ended', () => {
-    isPlayingNative.value = false
-  })
-  audio.addEventListener('emptied', () => {
-    isPlayingNative.value = false
-  })
+  }
+
+  watch(
+    audio,
+    (newAudio, oldAudio) => {
+      if (oldAudio) {
+        oldAudio.removeEventListener('play', onPlay)
+        oldAudio.removeEventListener('pause', onStop)
+        oldAudio.removeEventListener('ended', onStop)
+        oldAudio.removeEventListener('emptied', onStop)
+      }
+      if (newAudio) {
+        newAudio.addEventListener('play', onPlay)
+        newAudio.addEventListener('pause', onStop)
+        newAudio.addEventListener('ended', onStop)
+        newAudio.addEventListener('emptied', onStop)
+      }
+    },
+    { immediate: true }
+  )
 
   const start = async () => {
+    if (!audio.value) return
     try {
-      await audio.play()
+      await audio.value.play()
     } catch (e: unknown) {
       const err = e as Error
       if (err.name === 'NotSupportedError') {
@@ -41,7 +46,7 @@ const useIsPlaying = (audio: HTMLAudioElement) => {
     }
   }
   const pause = () => {
-    audio.pause()
+    audio.value?.pause()
   }
 
   const isPlaying = computed({
@@ -60,76 +65,93 @@ const useIsPlaying = (audio: HTMLAudioElement) => {
   return { wasUnsupportedType, isPlaying }
 }
 
-export const useCurrentTime = (
-  audio: Ref<HTMLAudioElement | undefined> | HTMLAudioElement
-) => {
-  const nativeCurrentTime = ref(toFinite(unref(audio)?.currentTime ?? 0, 0))
+export const useCurrentTime = (audio: Ref<HTMLAudioElement | undefined>) => {
+  const nativeCurrentTime = ref(toFinite(audio.value?.currentTime ?? 0, 0))
 
-  const setupTimeUpdate = (audio: HTMLAudioElement) => {
-    audio.addEventListener('timeupdate', () => {
-      nativeCurrentTime.value = toFinite(audio.currentTime, 0)
-    })
+  const onTimeupdated = () => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    nativeCurrentTime.value = toFinite(audio.value!.currentTime, 0)
   }
 
-  if (isRef(audio)) {
-    onMounted(() => {
-      if (!audio.value) return
-      setupTimeUpdate(audio.value)
-    })
-  } else {
-    setupTimeUpdate(audio)
-  }
+  watch(
+    audio,
+    (newAudio, oldAudio) => {
+      if (oldAudio) {
+        oldAudio.removeEventListener('timeupdate', onTimeupdated)
+      }
+      if (newAudio) {
+        newAudio.addEventListener('timeupdate', onTimeupdated)
+      }
+    },
+    { immediate: true }
+  )
 
   const currentTime = computed<number>({
     get() {
       return nativeCurrentTime.value
     },
     set(v) {
-      const a = unref(audio)
-      if (a) {
-        a.currentTime = v
-      }
+      if (!audio.value) return
+      audio.value.currentTime = v
     }
   })
 
   return currentTime
 }
 
-export const useDuration = (
-  audio: Ref<HTMLAudioElement | undefined> | HTMLAudioElement
-) => {
-  const nativeDuration = ref(toFinite(unref(audio)?.duration ?? 0, 0))
+export const useDuration = (audio: Ref<HTMLAudioElement | undefined>) => {
+  const nativeDuration = ref(toFinite(audio.value?.duration ?? 0, 0))
 
-  const setupLoadedMetadata = (audio: HTMLAudioElement) => {
-    audio.addEventListener('loadedmetadata', () => {
-      nativeDuration.value = toFinite(audio.duration, 0)
-    })
+  const onLoadedMetadata = () => {
+    if (!audio.value) return
+    nativeDuration.value = toFinite(audio.value.duration, 0)
   }
 
-  if (isRef(audio)) {
-    onMounted(() => {
-      if (!audio.value) return
-      setupLoadedMetadata(audio.value)
-    })
-  } else {
-    setupLoadedMetadata(audio)
-  }
+  watch(
+    audio,
+    (newAudio, oldAudio) => {
+      if (oldAudio) {
+        oldAudio.removeEventListener('loadedmetadata', onLoadedMetadata)
+      }
+      if (newAudio) {
+        newAudio.addEventListener('loadedmetadata', onLoadedMetadata)
+      }
+    },
+    { immediate: true }
+  )
 
   const duration = readonly(nativeDuration)
   return duration
 }
 
-const useVolume = (audio: HTMLAudioElement) => {
-  const nativeVolume = ref(toFinite(audio.volume, 1))
-  audio.addEventListener('volumechange', () => {
-    nativeVolume.value = toFinite(audio.volume, 1)
-  })
+const useVolume = (audio: Ref<HTMLAudioElement | undefined>) => {
+  const nativeVolume = ref(toFinite(audio.value?.volume ?? 1, 1))
+
+  const onVolumeChange = () => {
+    if (!audio.value) return
+    nativeVolume.value = toFinite(audio.value.volume, 1)
+  }
+
+  watch(
+    audio,
+    (newAudio, oldAudio) => {
+      if (oldAudio) {
+        oldAudio.removeEventListener('volumechange', onVolumeChange)
+      }
+      if (newAudio) {
+        newAudio.addEventListener('volumechange', onVolumeChange)
+      }
+    },
+    { immediate: true }
+  )
+
   const volume = computed<number>({
     get() {
       return nativeVolume.value
     },
     set(v) {
-      audio.volume = v / 100
+      if (!audio.value) return
+      audio.value.volume = v / 100
     }
   })
   return volume
@@ -141,17 +163,17 @@ const useAudio = (
 ) => {
   const { isPinPShown, showPictureInPictureWindow } = usePictureInPicture()
 
-  const audio = new Audio()
+  const audio = shallowRef(new Audio())
 
   const cantPlay = computed(
-    () => fileMeta.value && audio.canPlayType(fileMeta.value.mime) === ''
+    () => fileMeta.value && audio.value.canPlayType(fileMeta.value.mime) === ''
   )
 
   watch(
     () => fileMeta.value?.mime + fileRawPath.value,
     () => {
-      if (audio.canPlayType(fileMeta.value?.mime ?? '')) {
-        audio.src = fileRawPath.value
+      if (audio.value.canPlayType(fileMeta.value?.mime ?? '')) {
+        audio.value.src = fileRawPath.value
       }
     },
     { immediate: true }
@@ -163,7 +185,7 @@ const useAudio = (
   const volume = useVolume(audio)
 
   const startPinP = (iconId: string) => {
-    showPictureInPictureWindow(audio, iconId)
+    showPictureInPictureWindow(audio.value, iconId)
   }
   return {
     cantPlay,
