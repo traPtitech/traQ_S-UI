abf0f5132678046942c9d815f5e1bc34af800b15
Kira Kawai <asymptote.k.k@gmail.com>
2021-05-22 12:04:51 +0900

    fix 重複の対処

--- src/store/app/mutations.ts
+++ src/store/app/mutations.ts
@@ -2,8 +2,10 @@ import { defineMutations } from 'direct-vuex'
 import { S } from './state'
 
 export const mutations = defineMutations<S>()({
-  addSearchHistory(state, history: string) {
-    state.searchHistories.unshift(history)
+  addSearchHistory(state, newHistory: string) {
+    const index = state.searchHistories.indexOf(newHistory)
+    if (index !== -1) state.searchHistories.splice(index, 1)
+    state.searchHistories.unshift(newHistory)
     state.searchHistories = state.searchHistories.slice(0, 5)
   }
 })
