b8e5878d485c8123a8d01768457365a67182d0c9
sapphi-red <green@sapphi.red>
2021-05-25 11:29:03 +0900

    OGPの表示数が合わないことがあった
    無駄なOGPの取得処理をskip

--- src/use/message/embeddings.ts
+++ src/use/message/embeddings.ts
@@ -6,20 +6,6 @@ import {
   isMessage,
   isExternalUrl
 } from '@/lib/util/guard/embeddingOrUrl'
-import { ExternalUrl } from '@traptitech/traq-markdown-it'
-
-const ignoredHostNamesSet = new Set<string>(
-  window.traQConfig.ogpIgnoreHostNames
-)
-
-const isIncludedHost = (url: ExternalUrl) => {
-  try {
-    const hostName = new URL(url.url).hostname
-    return !ignoredHostNamesSet.has(hostName)
-  } catch {
-    return false // 不正なURL
-  }
-}
 
 const useEmbeddings = (props: { messageId: MessageId }) => {
   const embeddingsMap = computed(() =>
@@ -32,14 +18,13 @@ const useEmbeddings = (props: { messageId: MessageId }) => {
     quoteMessageIds: computed(
       () => embeddingsMap.value?.filter(isMessage).map(e => e.id) ?? []
     ),
-    externalUrls: computed(() => [
-      ...new Set(
-        embeddingsMap.value
-          ?.filter(isExternalUrl)
-          .filter(isIncludedHost)
-          .map(e => e.url) ?? []
-      )
-    ])
+    externalUrls: computed(() =>
+      [
+        ...new Set(
+          embeddingsMap.value?.filter(isExternalUrl).map(e => e.url) ?? []
+        )
+      ].slice(0, 2)
+    )
   })
   return { embeddingsState: state }
 }
