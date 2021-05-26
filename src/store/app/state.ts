fcab1990e28ba375c3c42687677be9fa8e673d90
Ras <66677201+Ras96@users.noreply.github.com>
2021-05-26 19:33:56 +0900

    Update src/store/app/state.ts
    Co-authored-by: 翠 / green <green@sapphi.red>

--- src/store/app/state.ts
+++ src/store/app/state.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/ban-types
 export type S = {
   /** 検索履歴 新しい順に並ぶ */
-  searchHistories: string[] //TODO 型
+  messageSearchHistories: string[]
 }
 
 export const state: S = {
