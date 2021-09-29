b21db0d4bc3149cdb77623405723f4cce7ed102e
sapphi-red <green@sapphi.red>
2021-09-29 23:39:17 +0900

    fix types

--- src/types/shims-vue-css-modules.d.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line unused-imports/no-unused-imports
-import { ComponentCustomProperties } from 'vue'
-
-declare module '@vue/runtime-core' {
-  interface ComponentCustomProperties {
-    $style: { [className: string]: string }
-  }
-}
