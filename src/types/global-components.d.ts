0a7f7c3e8c48c64e0ae419248650b0a53c98c5d9
sapphi-red <green@sapphi.red>
2021-10-02 22:31:40 +0900

    declare globalComponent

---
+++ src/types/global-components.d.ts
@@ -0,0 +1,8 @@
+declare module 'vue' {
+  export interface GlobalComponents {
+    RouterLink: typeof import('vue-router')['RouterLink']
+    RouterView: typeof import('vue-router')['RouterView']
+  }
+}
+
+export {}
