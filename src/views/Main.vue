d031dda7defc45af0a079dfeaaac5a4df1d46338
trasta <trasta298@gmail.com>
2021-10-14 11:59:05 +0900

    Revert "モバイルで右サイドバーを閉じるスワイプが効かなかった"
    This reverts commit abb403c2d19235767cd7f154b519f818d6bc2939.

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -279,7 +279,7 @@ $nav-width-ratio: math.div($nav-width-diff, $nav-width-display-width-diff);
   z-index: $z-index-sidebar-wrapper;
 }
 .sidebarPortal {
-  width: 320px;
+  width: 100%;
   height: 100%;
 }
 .mainView {
