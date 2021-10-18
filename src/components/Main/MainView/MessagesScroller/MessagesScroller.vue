7cc952727e33fd95604449dfc71dd43706db9270
sapphi-red <green@sapphi.red>
2021-10-19 01:29:17 +0900

    連続読み込みを防止するために、読み込み前に慣性スクロールを止めるように

--- src/components/Main/MainView/MessagesScroller/MessagesScroller.vue
+++ src/components/Main/MainView/MessagesScroller/MessagesScroller.vue
@@ -303,6 +303,9 @@ export default defineComponent({
 
       if (props.isLoading) return
       if (state.scrollTop < LOAD_MORE_THRESHOLD && !props.isReachedEnd) {
+        // 連続読み込みを防止するために、読み込み前に慣性スクロールを止める
+        rootRef.value.scrollTop = scrollTop
+
         emit('requestLoadFormer')
       }
       if (
