bc28e0788e4ec1bf35ff04d906c75befa74f27ed
sapphi-red <green@sapphi.red>
2021-11-07 21:24:28 +0900

    cypressのファイルをtsに

--- tests/e2e/support/commands.ts
+++ tests/e2e/support/commands.ts
@@ -7,22 +7,15 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+declare namespace Cypress {
+  interface Chainable {
+    login(): undefined
+    logout(): undefined
+    disableSW(): undefined
+  }
+}
 
 Cypress.Commands.add('login', () => {
   cy.request('POST', '/api/v3/login', {
@@ -37,6 +30,7 @@ Cypress.Commands.add('logout', () => {
 // disable service workers
 Cypress.Commands.add('disableSW', () => {
   Cypress.on('window:before:load', win => {
-    delete win.navigator.__proto__.serviceWorker
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    delete (win.navigator as any).__proto__.serviceWorker
   })
 })
