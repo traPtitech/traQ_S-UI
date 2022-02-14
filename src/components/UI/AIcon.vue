2885e76a3c7d7fb6f76bb6a83404d21f5659c787
sapphi-red <green@sapphi.red>
2022-02-14 21:22:47 +0900

    Rewrite icon import.meta.glob

--- src/components/UI/AIcon.vue
+++ src/components/UI/AIcon.vue
@@ -27,21 +27,27 @@ import { defineComponent, shallowRef, watch } from 'vue'
 import type { Component } from 'vue'
 import mdi from '/@/assets/mdi'
 
-// TODO: https://github.com/vitejs/vite/issues/5304
-const iconModules0 = import.meta.glob('/src/assets/icons/*.svg')
-const iconModules1 = import.meta.glob('/src/assets/icons/*/*.svg')
+type ComponentModule = {
+  default: Component
+}
+
+const iconModules0 = import.meta.glob<ComponentModule>(
+  '/src/assets/icons/*.svg',
+  {
+    assert: { type: 'component' }
+  }
+)
+const iconModules1 = import.meta.glob<ComponentModule>(
+  '/src/assets/icons/*/*.svg',
+  {
+    assert: { type: 'component' }
+  }
+)
 
 const iconModules = {
   ...iconModules0,
   ...iconModules1
-} as Record<
-  string,
-  () => Promise<
-    Readonly<{
-      default: Component
-    }>
-  >
->
+}
 
 export default defineComponent({
   name: 'AIcon',
