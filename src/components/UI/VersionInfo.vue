348cb7de30550c4b9e817708130f4577b875953b
sapphi-red <green@sapphi.red>
2022-03-11 17:51:12 +0900

    手動アプデチェックができるように
    とりあえず隠し機能

--- src/components/UI/VersionInfo.vue
+++ src/components/UI/VersionInfo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="$style.container">
+  <div :class="$style.container" @click="onClick">
     <div :class="$style.title">traQ</div>
     <div>Client: v{{ clientVersion }}</div>
     <div>Server: {{ serverVersion ?? '-----' }}</div>
@@ -7,18 +7,46 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue'
+import { computed, defineComponent, ref, watchEffect } from 'vue'
 import { VERSION } from '/@/lib/define'
 import useVersion from '/@/use/version'
+
+const useManualUpdateCheck = () => {
+  const threshold = 5
+  const checkUpdate = async () => {
+    const r = await navigator.serviceWorker.getRegistration()
+    await r?.update()
+
+    // eslint-disable-next-line no-console
+    console.log('Ran update check')
+  }
+
+  const clickedCount = ref(0)
+  const onClick = () => {
+    clickedCount.value++
+  }
+  watchEffect(() => {
+    if (clickedCount.value > threshold) {
+      checkUpdate()
+      clickedCount.value = 0
+    }
+  })
+
+  return { onClick }
+}
 
 export default defineComponent({
   name: 'VersionInfo',
   setup() {
     const { version: serverVersionData } = useVersion()
     const serverVersion = computed(() => serverVersionData.value?.version)
+
+    const { onClick } = useManualUpdateCheck()
+
     return {
       clientVersion: VERSION,
-      serverVersion
+      serverVersion,
+      onClick
     }
   }
 })
