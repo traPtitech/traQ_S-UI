a9b8da4284085d0f49a9668d24d57b8bbb2fef42
sapphi-red <green@sapphi.red>
2022-04-02 17:34:10 +0900

    fix for @typescript-eslint/consistent-type-imports

--- src/types/global-components.d.ts
+++ src/types/global-components.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/consistent-type-imports */
 /**
  * volar global components
  * @see https://github.com/johnsoncodehk/volar/blob/37deac779c407d5de16275fd7f341d1e11b6ad60/extensions/vscode-vue-language-features/README.md#usage
