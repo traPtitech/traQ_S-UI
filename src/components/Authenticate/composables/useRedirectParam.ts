604bd3fef052e42549ec6122dbb5e793f2afe449
sapphi-red <green@sapphi.red>
2022-04-11 15:42:07 +0900

    fix url refs #3242

--- src/components/Authenticate/composables/useRedirectParam.ts
+++ src/components/Authenticate/composables/useRedirectParam.ts
@@ -17,9 +17,9 @@ const useRedirectParam = () => {
     try {
       const u = new URL(url.value, location.href)
       if (u.origin === location.origin) {
-        // /api/v3/oauth/authorize はrouter.replaceではなくサーバーへのGETが必要
+        // /api/v3/oauth2/authorize はrouter.replaceではなくサーバーへのGETが必要
         // ref: https://github.com/traPtitech/traQ/pull/1413
-        if (u.pathname === '/api/v3/oauth/authorize') {
+        if (u.pathname === '/api/v3/oauth2/authorize') {
           location.href = url.value
           return
         }
