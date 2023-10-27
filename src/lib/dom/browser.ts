9e241228cf2b764127e5240af3b117c9ee93520c
mehm8128 <mehm8128@gmail.com>
2023-10-27 17:54:59 +0900

    macOSでisIOSがtrueにならないように

--- src/lib/dom/browser.ts
+++ src/lib/dom/browser.ts
@@ -11,7 +11,7 @@ export const isIOS = () => {
     ua.includes('iphone') ||
     ua.includes('ipod') ||
     ua.includes('ipad') ||
-    ua.includes('macintosh')
+    (ua.includes('macintosh') && 'ontouchend' in document)
   )
 }
 
