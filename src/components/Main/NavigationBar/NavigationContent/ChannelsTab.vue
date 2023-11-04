69f33a335d124df734233d5ab9ec08fbc6ebb2b8
mehm8128 <mehm8128@gmail.com>
2023-11-04 15:01:35 +0900

    チャンネルタブのタブをいい感じに

--- src/components/Main/NavigationBar/NavigationContent/ChannelsTab.vue
+++ src/components/Main/NavigationBar/NavigationContent/ChannelsTab.vue
@@ -15,6 +15,8 @@
         <channel-list-selector
           v-if="query.length === 0"
           v-model:is-stared="filterStarChannel"
+          :all-panel-id="allPanelId"
+          :stared-panel-id="staredPanelId"
         />
         <template v-if="topLevelChannels.length > 0">
           <channel-list
@@ -25,12 +27,25 @@
           <template v-else-if="filterStarChannel">
             <channel-tree-component
               v-if="staredChannels.length > 0"
+              :id="staredPanelId"
               :channels="staredChannels"
               show-shortened-path
+              role="tabpanel"
             />
-            <empty-state v-else>お気に入りチャンネルはありません</empty-state>
+            <empty-state
+              v-else
+              :id="staredPanelId"
+              role="tabpanel"
+            >
+              お気に入りチャンネルはありません
+            </empty-state>
           </template>
-          <channel-tree-component v-else :channels="topLevelChannels" />
+          <channel-tree-component
+            v-else
+            :id="allPanelId"
+            :channels="topLevelChannels"
+            role="tabpanel"
+          />
         </template>
         <empty-state v-else>チャンネルがありません</empty-state>
       </template>
@@ -58,6 +73,7 @@ import useStaredChannelDescendants from './composables/useStaredChannelDescendan
 import { useStaredChannels } from '/@/store/domain/staredChannels'
 import useChannelPath from '/@/composables/useChannelPath'
 import ChannelListSelector from '../ChannelList/ChannelListSelector.vue'
+import { randomString } from '/@/lib/basic/randomString'
 
 const { pushModal } = useModalStore()
 
@@ -110,6 +126,9 @@ const onClickButton = () => {
     type: 'channel-create'
   })
 }
+
+const allPanelId = randomString()
+const staredPanelId = randomString()
 </script>
 
 <style lang="scss" module>
