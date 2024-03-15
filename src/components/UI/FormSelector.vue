cd3942fe3e0244b7b72860c54bd4900fdd1cc31a
rei <h.watanabe360@gmail.com>
2024-03-15 15:18:39 +0900

    filteredOptionsを用いる形に変更 type Optionを作成 検索結果0のときの表示を作成 cssを仮作成

--- src/components/UI/FormSelector.vue
+++ src/components/UI/FormSelector.vue
@@ -5,38 +5,40 @@
     </label>
     <label v-if="activateSearch" :class="$style.search">
       <a-icon name="search" mdi />
-      <input v-model="searchText" />
+      <input v-model="filterText" />
     </label>
     <div
+      v-if="filteredOptions.length > 0"
       :class="$style.inputContainer"
       :data-on-secondary="$boolAttr(onSecondary)"
     >
       <select :id="id" v-model="value" :class="$style.select">
-        <template v-for="option in options" :key="option.value ?? nullSymbol">
-          <option
-            v-if="option.key.match(searchText)"
-            :value="option.value"
-            :disabled="option.value === null"
-          >
-            {{ option.key }}
-          </option>
-        </template>
+        <option
+          v-for="option in filteredOptions"
+          :key="option.value ?? nullSymbol"
+          :value="option.value"
+          :disabled="option.value === null"
+        >
+          {{ option.key }}
+        </option>
       </select>
     </div>
+    <span v-else :class="$style.errorText"> 候補が見つかりませんでした </span>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { randomString } from '/@/lib/basic/randomString'
 import { useModelValueSyncer } from '/@/composables/useModelSyncer'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import AIcon from '/@/components/UI/AIcon.vue'
 
+type Option = { key: string; value: string | null }
 const props = withDefaults(
   defineProps<{
     modelValue?: string | null
     onSecondary?: boolean
-    options: Array<{ key: string; value: string | null }>
+    options: Array<Option>
     label?: string
     activateSearch?: boolean
   }>(),
@@ -56,7 +58,12 @@ const nullSymbol = Symbol('null')
 const value = useModelValueSyncer(props, emit)
 const id = randomString()
 
-const searchText = ref('')
+const filterText = ref('')
+const filteredOptions = computed(() =>
+  props.options.filter((option: Option) =>
+    option.key.includes(filterText.value)
+  )
+)
 </script>
 
 <style lang="scss" module>
@@ -89,7 +96,23 @@ const searchText = ref('')
   background: inherit;
 }
 .search {
+  @include color-ui-primary;
+  @include background-secondary;
+  @include size-body1;
+  height: 30px;
   display: flex;
   align-items: center;
+  border-radius: 4px;
+  border: solid 2px transparent;
+}
+.errorText {
+  @include color-ui-primary;
+  @include background-secondary;
+  @include size-body1;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  border-radius: 4px;
+  border: solid 2px transparent;
 }
 </style>
