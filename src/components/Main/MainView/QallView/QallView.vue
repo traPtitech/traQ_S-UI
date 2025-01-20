67562d81a57e9016050e6352deb5afe694d716bd
masky5859 <mathsuky2406@gmail.com>
2025-01-20 23:58:15 +0900

    feat:通話周りのボタン用コンポーネントを作成

--- src/components/Main/MainView/QallView/QallView.vue
+++ src/components/Main/MainView/QallView/QallView.vue
@@ -2,8 +2,13 @@
 import { useQall } from '/@/composables/qall/useQall'
 import VideoComponent from '/@/components/Main/MainView/QallView/VideoTrack.vue'
 import AudioComponent from '/@/components/Main/MainView/QallView/AudioTrack.vue'
+import CallControlButton from './CallControlButton.vue'
 
-const { tracksMap } = useQall()
+const { tracksMap, toggleCalling } = useQall()
+
+const endCall = () => {
+  toggleCalling('') // Pass empty string since the channel name isn't needed for leaving
+}
 </script>
 
 <template>
@@ -22,6 +27,10 @@ const { tracksMap } = useQall()
         <AudioComponent
           v-else-if="track.trackPublication?.kind === 'audio'"
           :track="track.trackPublication.audioTrack!"
+        />
+        <CallControlButton
+          icon="/@/assets/icons/call_off.svg?url"
+          :on-click="endCall"
         />
       </template>
     </div>
