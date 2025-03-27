8f340c751c1a5425d39a8dfea0acc771f669b34e
Pugma <maronmarondelicious@gmail.com>
2025-03-28 07:37:24 +0900

    refactor: moved qall ws listener to existing one

--- src/composables/qall/useQall.ts
+++ src/composables/qall/useQall.ts
@@ -6,30 +6,18 @@ import {
 import { useMeStore } from '/@/store/domain/me'
 import { useToastStore } from '/@/store/ui/toast'
 import type { LocalAudioTrack, LocalVideoTrack } from 'livekit-client'
-import AutoReconnectWebSocket from '/@/lib/websocket/AutoReconnectWebSocket'
-import type { Channel, User } from '@traptitech/traq'
+import type {
+  Channel,
+  QallRoomStateChangedEventState,
+  User
+} from '@traptitech/traq'
 import { useChannelsStore } from '/@/store/entities/channels'
 import { useUsersStore } from '/@/store/entities/users'
 import { messageMitt } from '/@/store/entities/messages'
 import { useTts } from '/@/store/app/tts'
 import { useRtcSettings } from '/@/store/app/rtcSettings'
-import apis from '/@/lib/apis'
-
-type RoomsWithParticipants =
-  | {
-      roomId: string
-      participants:
-        | {
-            identity: string
-            joinedAt: string
-            name: string
-            canPublish: boolean
-            attributes: { [key: string]: string }
-          }[]
-        | null
-      isWebinar: boolean
-    }[]
-  | null
+import { wsListener } from '/@/lib/websocket'
+import useMittListener from '../utils/useMittListener'
 
 type Participant = {
   user: User
@@ -45,6 +33,10 @@ type Room = {
 type Rooms = Room[]
 
 const rooms = ref<Rooms>([])
+
+useMittListener(wsListener, 'QALL_ROOM_STATE_CHANGED', async ({ state }) => {
+  purifyRoomData([state]).then(purifiedRooms => (rooms.value = purifiedRooms))
+})
 
 const {
   joinRoom,
@@ -71,24 +63,28 @@ const { addQueue } = useTts()
 const meStore = useMeStore()
 const rtcSettings = useRtcSettings()
 
-const purifyRoomData = async (data: RoomsWithParticipants): Promise<Rooms> => {
+const purifyRoomData = async (
+  data: QallRoomStateChangedEventState[]
+): Promise<Rooms> => {
   if (!data) return []
   await bothChannelsMapInitialFetchPromise.value
   return data
     .filter(room => room.participants && room.participants.length > 0)
     .map(room => {
       return {
-        channel: channelsMap.value.get(room.roomId),
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        channel: channelsMap.value.get(room.roomId!),
         participants:
           room.participants
             ?.map(p => ({
               joinedAt: p.joinedAt,
-              user: findUserByName(p.identity.slice(0, -37)),
-              canPublish: p.canPublish ?? false,
-              attributes: p.attributes ?? {}
+              // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+              user: findUserByName(p.identity!.slice(0, -37)),
+              canPublish: p.canPublish,
+              attributes: p.attributes
             }))
             .filter((p): p is Participant => !!p.user) ?? [],
-        isWebinar: room.isWebinar ?? false
+        isWebinar: room.isWebinar
       }
     })
     .filter((room): room is Room => {
@@ -96,24 +92,6 @@ const purifyRoomData = async (data: RoomsWithParticipants): Promise<Rooms> => {
       return !room.channel.archived
     })
 }
-const ws = new AutoReconnectWebSocket(
-  (await apis.getQallEndpoints()).data.endpoint,
-  undefined,
-  {
-    maxReconnectionDelay: 3000,
-    minReconnectionDelay: 1000
-  }
-)
-ws.addEventListener('message', async event => {
-  try {
-    const data: RoomsWithParticipants = JSON.parse(event.detail as string)
-    rooms.value = await purifyRoomData(data)
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.warn('[WebSocket] Failed to parse: ', e)
-  }
-})
-ws.connect()
 
 const setSpeakerMute = (track: LocalAudioTrack, muted: boolean) => {
   setLocalTrackMute(track, muted)
