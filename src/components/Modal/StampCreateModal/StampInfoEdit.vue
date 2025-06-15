b9d345281959f48b9139a702140a3889929948b1
Oxojo <oxojo9x@gmail.com>
2025-06-15 12:42:05 +0900

    fix: スタンプ名に使えない文字が含まれているときに登録ボタンを押せないように

--- src/components/Modal/StampCreateModal/StampInfoEdit.vue
+++ src/components/Modal/StampCreateModal/StampInfoEdit.vue
@@ -22,6 +22,7 @@
       <form-button label="戻る" type="tertiary" @click="back" />
       <form-button
         label="登録する"
+        :disabled="!isNameValid"
         :loading="isCreating"
         :class="$style.form"
         @click="createStamp"
@@ -39,6 +40,7 @@ import FormButton from '/@/components/UI/FormButton.vue'
 import { useMeStore } from '/@/store/domain/me'
 import { useModalStore } from '/@/store/ui/modal'
 import type { AxiosError } from 'axios'
+import { isValidStampName } from '/@/lib/validate'
 
 const props = defineProps<{
   stampImage: File
@@ -54,6 +56,8 @@ const trimExt = (filename: string) => filename.replace(/\.[^.]+$/, '')
 
 const imageUrl = computed(() => URL.createObjectURL(props.stampImage))
 const newStampName = ref(trimExt(props.stampImage.name))
+
+const isNameValid = computed(() => isValidStampName(newStampName.value))
 
 const useStampCreate = (newStampName: Ref<string>, stampImage: File) => {
   const { addSuccessToast, addErrorToast } = useToastStore()
