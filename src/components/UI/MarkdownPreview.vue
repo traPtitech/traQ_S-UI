963c78a4c365f89bc9c33b56d803835ec66a11fc
Fto4 <taishi2007futoshi@gmail.com>
2025-07-03 14:49:31 +0900

    MarkdownPreviewの作成

---
+++ src/components/UI/MarkdownPreview.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    class="markdown-body"
+    :class="$style.content"
+    :data-accept-action="acceptAction"
+    @click="toggleSpoilerHandler"
+    v-html="renderedContent"
+  />
+</template>
+
+<script lang="ts" setup>
+import { render } from '/@/lib/markdown/markdown'
+import type { MarkdownRenderResult } from '@traptitech/traq-markdown-it'
+import { computed, nextTick, ref, watchEffect } from 'vue'
+import useSpoilerToggler from '/@/composables/markdown/useSpoilerToggler'
+
+const props = withDefaults(
+  defineProps<{
+    content?: string
+    acceptAction?: boolean
+  }>(),
+  {
+    content: '',
+    acceptAction: false
+  }
+)
+const emit = defineEmits<{
+  render: []
+}>()
+
+const rendered = ref<MarkdownRenderResult>()
+watchEffect(async () => {
+  rendered.value = await render(props.content)
+  await nextTick()
+  emit('render')
+})
+const renderedContent = computed(() => rendered.value?.renderedText)
+
+const { toggleSpoilerHandler } = useSpoilerToggler()
+</script>
+
+<style lang="scss" module>
+.content {
+  word-break: break-all;
+  &[data-accept-action='false'] {
+    pointer-events: none;
+  }
+}
+</style>
