69910d0a096a687bf51421203c2b54213c8b0c64
Fto4 <taishi2007futoshi@gmail.com>
2025-07-03 14:57:24 +0900

    もとに戻した

--- src/views/Settings/ProfileTab.vue
+++ src/views/Settings/ProfileTab.vue
@@ -19,7 +19,7 @@
     <section :class="$style.section">
       <h3 :class="$style.heading">ひとこと</h3>
       <div :class="$style.bioContainer">
-        <markdown-content :content="previewRendered" />
+        <inline-markdown :content="state.bio" accept-action />
         <form-text-area v-model="state.bio" rows="2" :max-length="1000" />
       </div>
     </section>
@@ -53,7 +53,7 @@
 
 <script lang="ts">
 import type { Ref } from 'vue'
-import { computed, reactive, ref, toRef, watchEffect } from 'vue'
+import { computed, reactive, ref, toRef } from 'vue'
 import type { UserDetail } from '@traptitech/traq'
 import apis, { formatResizeError } from '/@/lib/apis'
 import useStateDiff from '/@/components/Settings/composables/useStateDiff'
@@ -137,8 +137,7 @@ import UserIcon from '/@/components/UI/UserIcon.vue'
 import FormInput from '/@/components/UI/FormInput.vue'
 import FormSelectorFilterable from '/@/components/UI/FormSelectorFilterable.vue'
 import FormTextArea from '/@/components/UI/FormTextArea.vue'
-import MarkdownContent from '/@/components/UI/MarkdownContent.vue'
-import { render } from '/@/lib/markdown/markdown'
+import InlineMarkdown from '/@/components/UI/InlineMarkdown.vue'
 
 const { detail: detailMayBeUndefined } = useMeStore()
 
@@ -194,12 +193,6 @@ onBeforeRouteLeave(() => {
   if (!result) {
     return false
   }
-})
-
-const previewRendered = ref('')
-watchEffect(async () => {
-  const { renderedText } = await render(state.bio)
-  previewRendered.value = renderedText
 })
 </script>
 
