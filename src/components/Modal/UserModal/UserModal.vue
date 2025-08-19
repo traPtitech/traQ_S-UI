50ef0f9f0194e4b78eabc64f35bdd0b8079d1a0a
queue113 <ym553232@gmail.com>
2025-08-20 04:29:09 +0900

    モーダルのユーザアイコンのカーソルを変更

--- src/components/Modal/UserModal/UserModal.vue
+++ src/components/Modal/UserModal/UserModal.vue
@@ -139,4 +139,9 @@ const isThisMyProfile = computed(() => props.id === myId.value)
   margin: auto;
   border: 6px solid $theme-background-secondary-border;
 }
+
+:global([data-testid='usermodal']) .icon[role='button'] {
+  cursor: default;
+}
+
 </style>
