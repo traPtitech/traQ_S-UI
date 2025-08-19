c525860c5e6dad5d6b0089a2a90b7dce47eba7ec
rei <107461411+reiroop@users.noreply.github.com>
2025-08-19 19:34:24 +0900

    feat: 通知設定モーダルから他ユーザーの通知設定を変更する機能を削除

--- src/components/Modal/NotificationModal/NotificationModal.vue
+++ src/components/Modal/NotificationModal/NotificationModal.vue
@@ -4,29 +4,16 @@
     :subtitle="channelPathString"
     icon-name="notified-or-subscribed"
   >
-    <modal-section title="自分の通知設定">
-      <notification-state-selector :channel-id="channelId" />
-    </modal-section>
-    <modal-section
-      title="他ユーザーの通知設定"
-      description="このチャンネルのメッセージの通知がユーザーに送られるか選択できます（通知を送る場合、未読管理も有効になります）"
-    >
-      <user-notification-list
-        v-if="channelId && modalMounted"
-        :channel-id="channelId"
-      />
-    </modal-section>
+    <notification-state-selector :channel-id="channelId" />
   </modal-frame>
 </template>
 
 <script lang="ts" setup>
+import { computed } from 'vue'
 import ModalFrame from '../Common/ModalFrame.vue'
-import ModalSection from '../Common/ModalSection.vue'
 import NotificationStateSelector from './NotificationStateSelector.vue'
-import UserNotificationList from './UserNotificationList.vue'
-import { computed, onMounted, ref } from 'vue'
-import type { ChannelId } from '/@/types/entity-ids'
 import useChannelPath from '/@/composables/useChannelPath'
+import type { ChannelId } from '/@/types/entity-ids'
 
 const props = defineProps<{
   channelId: ChannelId
@@ -36,14 +23,6 @@ const { channelIdToPathString } = useChannelPath()
 const channelPathString = computed(() =>
   channelIdToPathString(props.channelId, true)
 )
-
-const modalMounted = ref(false)
-
-onMounted(() => {
-  setTimeout(() => {
-    modalMounted.value = true
-  }, 0)
-})
 </script>
 
 <style lang="scss" module></style>
