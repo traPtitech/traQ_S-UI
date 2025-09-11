22712d55d2a00dffc60f36bd24ef0d1857374f13
uni-kakurenbo <kohma1116@gmail.com>
2025-09-11 20:24:53 +0900

    feat: 移動先の message id が移動前のものと等しいとき，history に push しない

---
+++ src/components/UI/MessageLink.vue
@@ -0,0 +1,19 @@
+<template>
+  <router-link
+    :to="constructMessagesPath(messageId)"
+    :replace="messageQuery === messageId"
+  >
+    <slot />
+  </router-link>
+</template>
+
+<script lang="ts" setup>
+import { constructMessagesPath } from '/@/router'
+import { useMessageQuery } from '/@/composables/router/useRoutes'
+
+const messageQuery = useMessageQuery()
+
+const { messageId } = defineProps<{
+  messageId: string
+}>()
+</script>
