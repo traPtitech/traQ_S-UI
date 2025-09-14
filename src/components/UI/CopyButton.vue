514d0e75f6583b7fa4c9d13e5f40629bb90aefcc
uni-kakurenbo <kohma1116@gmail.com>
2025-09-14 19:14:40 +0900

    feat: `CopyButton` component を作成

---
+++ src/components/UI/CopyButton.vue
@@ -0,0 +1,65 @@
+<template>
+  <IconButton
+    :class="$style.button"
+    icon-mdi
+    :icon-name="
+      copied
+        ? 'clipboard-check-multiple-outline'
+        : 'clipboard-text-multiple-outline'
+    "
+    :title="copied ? 'copied!' : 'copy'"
+    @click.stop="copy"
+    @mouseenter="onMouseEnter"
+    @mouseleave="onMouseLeave"
+  />
+</template>
+
+<script lang="ts" setup>
+import { ref, toValue, type MaybeRefOrGetter } from 'vue'
+import IconButton from '/@/components/UI/IconButton.vue'
+import useHover from '/@/composables/dom/useHover'
+import useExecWithToast from '/@/composables/toast/useExecWithToast'
+
+const { contentsSource } = defineProps<{
+  contentsSource: MaybeRefOrGetter<string | undefined | null>
+}>()
+
+const { execWithToast } = useExecWithToast()
+const copied = ref(false)
+const { isHovered, onMouseEnter, onMouseLeave: onMouseLeaveImpl } = useHover()
+
+let timeout: NodeJS.Timeout | null = null
+
+const onMouseLeave = () => {
+  onMouseLeaveImpl()
+  if (!timeout) copied.value = false
+}
+
+const copy = async () => {
+  const content = toValue(contentsSource)
+  if (!content) return
+
+  execWithToast(undefined, 'コピーに失敗しました', async () => {
+    await navigator.clipboard.writeText(content)
+    copied.value = true
+
+    timeout = setTimeout(() => {
+      timeout = null
+      if (isHovered.value) return
+      copied.value = false
+    }, 1500)
+  })
+}
+</script>
+
+<style lang="scss" module>
+.button {
+  @include color-ui-primary;
+
+  opacity: 0.6;
+
+  &:hover {
+    opacity: 1;
+  }
+}
+</style>
