6b887e5336d9d85520fa90560a89cd18e8604291
tufusa <me@tufusa.net>
2025-10-05 10:55:59 +0900

    feat: fetchMe の失敗時にリトライ処理を追加

--- src/store/domain/me.ts
+++ src/store/domain/me.ts
@@ -7,6 +7,8 @@ import useIndexedDbValue from '/@/composables/utils/useIndexedDbValue'
 import { deleteToken } from '/@/lib/notification/notification'
 import { wsListener } from '/@/lib/websocket'
 import apis from '/@/lib/apis'
+import { isAxiosError } from 'axios'
+import { useRouter } from 'vue-router'
 
 export type IDBState = {
   detail: Readonly<MyUserDetail> | undefined
@@ -16,21 +18,41 @@ const useMeStorePinia = defineStore('domain/me', () => {
   const initialValue: IDBState = {
     detail: undefined
   }
+  const router = useRouter()
 
   // TODO: ログインチェック時にrestoreを待つ必要があるかもしれない
   const [state] = useIndexedDbValue('store/domain/me', 1, {}, initialValue)
 
   const myId = computed(() => state.detail?.id)
 
-  const fetchMe = async () => {
-    try {
-      const { data } = await apis.getMe()
-      state.detail = data
-      return data
-    } catch {
-      state.detail = undefined
-      return undefined
+  const fetchMe = async (): Promise<MyUserDetail | undefined> => {
+    const retryDelayMs = 1000 // 1 sec
+    const retryMaxCount = 10
+
+    for (let i = 0; i < retryMaxCount; i++) {
+      try {
+        const { data } = await apis.getMe()
+        state.detail = data
+        return data
+      } catch (error) {
+        state.detail = undefined
+
+        if (!isAxiosError(error)) {
+          console.error(new Error('Failed to fetchMe:', { cause: error }))
+          return undefined
+        }
+        if (error.status === 401) {
+          router.push('/login')
+          return undefined
+        }
+
+        console.warn('Failed to fetchMe, retrying...', { cause: error })
+        await new Promise(resolve => setTimeout(resolve, retryDelayMs))
+      }
     }
+
+    console.error(new Error(`Failed to fetchMe after ${retryMaxCount} retry`))
+    return undefined
   }
   const logout = async ({
     allSession = false
