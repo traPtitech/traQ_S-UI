8bf6d5f68278adc7c1c2ca0d07fc65d41c23b245
otukado <otukado@gmail.com>
2025-10-27 17:45:28 +0900

    refactor: `responsive.ts` を `Pinia` から `effectScope` に移植

--- src/store/ui/responsive.ts
+++ src/store/ui/responsive.ts
@@ -1,9 +1,9 @@
-import { defineStore, acceptHMRUpdate } from 'pinia'
-import { ref } from 'vue'
+import { ref, effectScope, readonly } from 'vue'
 import { mobileMinBreakpoint } from '/@/lib/media'
-import { convertToRefsStore } from '/@/store/utils/convertToRefsStore'
 
-const useResponsiveStorePinia = defineStore('ui/responsive', () => {
+const scope = effectScope()
+
+const responsiveState = scope.run(() => {
   const queryList = window.matchMedia(`(max-width: ${mobileMinBreakpoint}px)`)
   const isHoverSupported = window.matchMedia(
     '(hover: hover) and (pointer: fine)'
@@ -11,20 +11,24 @@ const useResponsiveStorePinia = defineStore('ui/responsive', () => {
 
   const isMobile = ref(queryList.matches)
   const isTouchDevice = ref(!isHoverSupported.matches)
+
   queryList.addEventListener('change', (event: MediaQueryListEvent) => {
     isMobile.value = event.matches
   })
   isHoverSupported.addEventListener('change', (event: MediaQueryListEvent) => {
-    isTouchDevice.value = event.matches
+    isTouchDevice.value = !event.matches
   })
 
   return { isMobile, isTouchDevice }
 })
 
-export const useResponsiveStore = convertToRefsStore(useResponsiveStorePinia)
-
-if (import.meta.hot) {
-  import.meta.hot.accept(
-    acceptHMRUpdate(useResponsiveStorePinia, import.meta.hot)
-  )
+if (!responsiveState) {
+  throw new Error('Failed to initialize responsive state')
 }
+
+const { isMobile, isTouchDevice } = responsiveState
+
+export const useResponsiveStore = () => ({
+  isMobile: readonly(isMobile),
+  isTouchDevice: readonly(isTouchDevice)
+})
