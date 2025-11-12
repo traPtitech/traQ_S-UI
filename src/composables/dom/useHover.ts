75ee76735230c0ac7c4711efa82a6838422f1759
uni-kakurenbo <kohma1116@gmail.com>
2025-11-12 09:40:42 +0900

    fix: timeout が多重にならないようにする

--- src/composables/dom/useHover.ts
+++ src/composables/dom/useHover.ts
@@ -3,18 +3,26 @@ import useToggle from '/@/composables/utils/useToggle'
 const useHover = (delay: number = 0) => {
   const { value, open, close } = useToggle()
   let timeoutId: NodeJS.Timeout | null = null
+  let shouldOpen = false
 
   const onMouseEnter = () => {
+    shouldOpen = true
+
     if (delay <= 0) {
       open()
     } else {
+      if (timeoutId) clearTimeout(timeoutId)
+
       timeoutId = setTimeout(() => {
-        open()
+        timeoutId = null
+        if (shouldOpen) open()
       }, delay)
     }
   }
 
   const onMouseLeave = () => {
+    shouldOpen = false
+
     if (timeoutId) {
       clearTimeout(timeoutId)
       timeoutId = null
