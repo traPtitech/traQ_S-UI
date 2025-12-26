2f219e8b007a306e1535af5655f7207cec0b8598
uni-kakurenbo <kohma1116@gmail.com>
2025-12-26 21:59:58 +0900

    Reapply "Merge pull request #4894 from traPtitech/fix/use-set-range-text"
    This reverts commit e0ece6b6f6c2ea5b3c004874ebcc68f5c7e50cf4.

---
+++ src/lib/dom/insertText.ts
@@ -0,0 +1,32 @@
+import { insert } from 'text-field-edit'
+
+import { isDefined } from '/@/lib/basic/array'
+
+export const insertText = (
+  textarea: HTMLTextAreaElement,
+  text: string,
+  target: {
+    begin?: number
+    end?: number
+  } = {},
+  isMobile = false
+) => {
+  // `execCommand` は deprecated だが，`setRangeText` は undo できなくなってしまうので，PC の場合は `execCommand` を用いる．
+  // `execCommand` は主にモバイル端末での挙動が怪しいので，それを改善するためのワークアラウンド．
+  if (isMobile) {
+    textarea.setRangeText(
+      text,
+      target?.begin ?? textarea.selectionStart,
+      target?.end ?? textarea.selectionEnd,
+      'end'
+    )
+  } else {
+    if (isDefined(target?.begin)) textarea.selectionStart = target.begin
+    if (isDefined(target?.end)) textarea.selectionEnd = target.end
+
+    // Windowsでの\r\nを含む文字列を貼り付けた後に
+    // Ctrl+Zでアンドゥすると、キャレットの位置がずれるので
+    // ずれないように\nに統一しておく
+    insert(textarea, text.replaceAll('\r\n', '\n'))
+  }
+}
