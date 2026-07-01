ad5abc32bb2ceabf47c71bed6e52ec5ba083266f
aorinngo <aorinngo@trap.jp>
2026-07-01 15:38:21 +0900

    グループの編集画面にスクロールされるように。

--- src/components/GroupManager/GroupListGroup.vue
+++ src/components/GroupManager/GroupListGroup.vue
@@ -1,10 +1,14 @@
 <template>
-  <GroupListGroupEdit v-if="isSelected" :group="group" />
-  <GroupListGroupView v-else :group="group" @click-edit="onSelect" />
+  <div ref="rootRef">
+    <GroupListGroupEdit v-if="isSelected" :group="group" />
+    <GroupListGroupView v-else :group="group" @click-edit="onSelect" />
+  </div>
 </template>
 
 <script lang="ts" setup>
 import type { UserGroup } from '@traptitech/traq'
+
+import { nextTick, ref, watch } from 'vue'
 
 import type { UserGroupId } from '/@/types/entity-ids'
 
@@ -25,7 +29,25 @@ const emit = defineEmits<{
   (e: 'select', _groupId: UserGroupId): void
 }>()
 
+const rootRef = ref<HTMLElement | null>(null)
+
 const onSelect = () => {
   emit('select', props.group.id)
 }
+
+watch(
+  () => props.isSelected,
+  async isSelected => {
+    if (!isSelected) return
+
+    await nextTick()
+
+    rootRef.value?.scrollIntoView({
+      block: 'nearest'
+    })
+  },
+  {
+    immediate: true
+  }
+)
 </script>
