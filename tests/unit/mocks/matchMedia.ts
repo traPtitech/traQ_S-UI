8ccae229bd59b07f7800c217d1b7234293e42829
uni-kakurenbo <kohma1116@gmail.com>
2026-07-02 15:06:19 +0900

    test: add tests

--- tests/unit/mocks/matchMedia.ts
+++ tests/unit/mocks/matchMedia.ts
@@ -1,11 +1,16 @@
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: vitest.fn().mockImplementation(query => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addEventListener: vitest.fn(),
-    removeEventListener: vitest.fn(),
-    dispatchEvent: vitest.fn()
-  }))
-})
+export const setupMatchMedia = (matches = false) => {
+  Object.defineProperty(window, 'matchMedia', {
+    configurable: true,
+    writable: true,
+    value: vitest.fn().mockImplementation(query => ({
+      matches,
+      media: query,
+      onchange: null,
+      addEventListener: vitest.fn(),
+      removeEventListener: vitest.fn(),
+      dispatchEvent: vitest.fn()
+    }))
+  })
+}
+
+setupMatchMedia()
