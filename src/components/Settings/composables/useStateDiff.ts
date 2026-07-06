e9badf681926d00db7ce1189dc12c88af752fe45
a-cuppa <169031030+a-cuppa@users.noreply.github.com>
2026-07-06 17:05:21 +0900

    feat: useStateDiffにおいて、getDiffKeys・hasDiffの第二引数にrefだけではなく生の値を渡せるように
     commit message for your changes. Lines starting

--- src/components/Settings/composables/useStateDiff.ts
+++ src/components/Settings/composables/useStateDiff.ts
@@ -1,21 +1,21 @@
-import type { Ref } from 'vue'
+import { type MaybeRef, toValue } from 'vue'
 
 import { dequal } from 'dequal'
 
 const useStateDiff = <T>() => {
   const getDiffKeys = (
     state: Readonly<Partial<T>>,
-    storeState: Readonly<Ref<T>>
+    storeState: Readonly<MaybeRef<T>>
   ) => {
     return (Object.keys(state) as Array<keyof T>).filter(key => {
       const k = key as keyof T
-      return !dequal(state[k], storeState.value[k])
+      return !dequal(state[k], toValue(storeState)[k])
     })
   }
 
   const hasDiff = (
     state: Readonly<Partial<T>>,
-    storeState: Readonly<Ref<T>>
+    storeState: Readonly<MaybeRef<T>>
   ): boolean => {
     return getDiffKeys(state, storeState).length > 0
   }
