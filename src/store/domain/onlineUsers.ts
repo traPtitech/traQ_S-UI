ab24eae134a7eb49fef039d47c1a5a4285a34304
uni-kakurenbo <kohma1116@gmail.com>
2026-09-04 20:15:48 +0900

    fix: preserve online updates during refresh

--- src/store/domain/onlineUsers.ts
+++ src/store/domain/onlineUsers.ts
@@ -3,16 +3,33 @@ import { readonly, ref } from 'vue'
 import { acceptHMRUpdate, defineStore } from 'pinia'
 
 import apis from '/@/lib/apis'
-import { createSingleflight } from '/@/lib/basic/async'
 import { wsListener } from '/@/lib/websocket'
 import { convertToRefsStore } from '/@/store/utils/convertToRefsStore'
 import type { UserId } from '/@/types/entity-ids'
 
-const getOnlineUsers = createSingleflight(apis.getOnlineUsers.bind(apis))
-
 const useOnlineUsersPinia = defineStore('domain/onlineUsers', () => {
   const onlineUsers = ref(new Set<UserId>())
   const onlineUsersFetched = ref(false)
+  let changesDuringFetch: Map<UserId, boolean> | undefined
+  let currentFetch: Promise<Set<UserId>> | undefined
+
+  const fetchOnlineUsersFromApi = async () => {
+    const changes = new Map<UserId, boolean>()
+    changesDuringFetch = changes
+    try {
+      const { data: userIdsArray } = await apis.getOnlineUsers()
+      const userIds = new Set(userIdsArray)
+      changes.forEach((online, id) => {
+        if (online) userIds.add(id)
+        else userIds.delete(id)
+      })
+      onlineUsers.value = userIds
+      onlineUsersFetched.value = true
+      return userIds
+    } finally {
+      changesDuringFetch = undefined
+    }
+  }
 
   const fetchOnlineUsers = async ({
     ignoreCache = false
@@ -21,24 +38,29 @@ const useOnlineUsersPinia = defineStore('domain/onlineUsers', () => {
       return onlineUsers.value
     }
 
-    const [{ data: userIdsArray }, shared] = await getOnlineUsers()
-    const userIds = new Set(userIdsArray)
-    if (!shared) {
-      onlineUsers.value = new Set(userIds)
-      onlineUsersFetched.value = true
+    if (currentFetch) return currentFetch
+
+    currentFetch = fetchOnlineUsersFromApi()
+    try {
+      return await currentFetch
+    } finally {
+      currentFetch = undefined
     }
-    return userIds
   }
 
   wsListener.on('USER_ONLINE', ({ id }) => {
     onlineUsers.value.add(id)
+    changesDuringFetch?.set(id, true)
   })
   wsListener.on('USER_OFFLINE', ({ id }) => {
     onlineUsers.value.delete(id)
+    changesDuringFetch?.set(id, false)
   })
 
-  wsListener.on('reconnect', () => {
-    fetchOnlineUsers({ ignoreCache: true })
+  wsListener.on('reconnect', async () => {
+    const pendingFetch = currentFetch
+    if (pendingFetch) await pendingFetch.catch(() => undefined)
+    await fetchOnlineUsers({ ignoreCache: true })
   })
 
   return {
