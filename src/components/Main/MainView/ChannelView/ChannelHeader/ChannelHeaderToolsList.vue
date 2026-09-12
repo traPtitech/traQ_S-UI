1828473baa146798c11cb4b3de068bc43a349ca8
aoringo0606 <68138415+aoringo0606@users.noreply.github.com>
2026-09-12 13:48:34 +0900

    refactor: いらないCSSを削除

--- src/components/Main/MainView/ChannelView/ChannelHeader/ChannelHeaderToolsList.vue
+++ src/components/Main/MainView/ChannelView/ChannelHeader/ChannelHeaderToolsList.vue
@@ -135,12 +135,6 @@ const { starChannel, unstarChannel } = useStarChannel(props)
 }
 .moreButton {
   position: relative;
-  display: inline;
-}
-.popupLocator {
-  position: absolute;
-  right: 0;
-  top: 100%;
 }
 .qallIcon {
   transition: transform 0.1s;
